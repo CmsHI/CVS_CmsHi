4871b257250bc559dd676d0c5c028bc14ffcd70e
edwenger <>
2009-05-14 10:12:35 +0000

    check out BRANCH22X for latest 2_2_x tracking code

--- Utilities/scripts/setup228.sh
+++ Utilities/scripts/setup228.sh
@@ -7,7 +7,7 @@ eval `scramv1 ru -sh`
 
 cvs co RecoHI/Configuration
 
-cvs co -r V01-00-02 RecoHI/HiTracking/python
+cvs co -r BRANCH22X RecoHI/HiTracking
 cvs co -r V02-02-06 RecoPixelVertexing/PixelLowPtUtilities
 
 cvs co RecoHI/HiJetAlgos
@@ -15,14 +15,18 @@ cvs co RecoHI/HiEgammaAlgos
 
 cvs co GeneratorInterface/HydjetInterface
 cvs co GeneratorInterface/PyquenInterface
+
 cvs co SimDataFormats/HiGenData
 cvs co SimDataFormats/HepMCProduct
 cvs co SimDataFormats/GeneratorProducts
+
 cvs co -r $CMSSW_VERSION SimCalorimetry/HcalSimProducers
 cat $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigiProducer.cc | replace "doZDC(true)" "doZDC(false)" | replace "e.getByLabel(\"mix\", zdcHitsName , zdccf)" "// e.getByLabel(\"mix\", zdcHitsName , zdccf)" | replace "colzdc(new MixCollection<PCaloHit>(zdccf.product()))" "colzdc(new MixCollection<PCaloHit>(new CrossingFrame<PCaloHit>))" | replace "theHitCorrection->fillChargeSums(*colzdc)" "// zdc correction" > tmp.cc
 mv tmp.cc $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigiProducer.cc
+
 cvs co UserCode/CmsHi
 mv UserCode/CmsHi .
+
 scramv1 b
 
 
