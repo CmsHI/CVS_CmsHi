1cc4878f5d28502aa3aaeed687f5e9858b065e7d
edwenger <>
2009-06-16 14:18:31 +0000

    fix for pre10

--- Utilities/scripts/setup31X.sh
+++ Utilities/scripts/setup31X.sh
@@ -5,11 +5,17 @@ export CVSROOT=:gserver:cmscvs.cern.ch:/cvs_server/repositories/CMSSW
 cd $CMSSW_BASE/src
 eval `scramv1 ru -sh`
 
+# Fix for 3_1_0_pre10
+if echo "$CMSSW_BASE" | grep "CMSSW_3_1_0_pre10" >/dev/null 2>&1
+then
+  echo You are using CMSSW_3_1_0_pre10.  Must patch pyqt.xml...
+  cat $CMSSW_BASE/config/toolbox/slc4_ia32_gcc345/tools/selected/pyqt.xml | sed -e '/environment.*PYTHONPATH/d' > temp.xml
+  mv temp.xml $CMSSW_BASE/config/toolbox/slc4_ia32_gcc345/tools/selected/pyqt.xml 
+  scramv1 setup pyqt; scramv1 b clean
+fi
+
 cvs co RecoHI/Configuration
-
-cvs co RecoHI/HiTracking                        # Good tag?
-#cvs co RecoPixelVertexing/PixelLowPtUtilities   # ?
-
+cvs co RecoHI/HiTracking                        
 cvs co RecoHI/HiJetAlgos
 cvs co RecoHI/HiEgammaAlgos
 cvs co RecoHI/HiCentralityAlgos
@@ -19,6 +25,7 @@ cvs co -r embedding_v02 GeneratorInterface/PyquenInterface
 
 cvs co SimDataFormats/HiGenData
 
+# Turn off ZDC digitization
 #cvs co -r $CMSSW_VERSION SimCalorimetry/HcalSimProducers
 #cat $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc | replace "doZDC(true)" "doZDC(false)" | replace "e.getByLabel(\"mix\", zdcHitsName , zdccf)" "// e.getByLabel(\"mix\", zdcHitsName , zdccf)" | replace "colzdc(new MixCollection<PCaloHit>(zdccf.product()))" "colzdc(new MixCollection<PCaloHit>(new CrossingFrame<PCaloHit>))" | replace "theHitCorrection->fillChargeSums(*colzdc)" "// zdc correction" > tmp.cc
 #mv tmp.cc $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc
