3f6a7436e049fef45aae7e77a0558744d838549d
yilmaz <>
2009-06-16 11:53:55 +0000

    some fixes

--- Utilities/scripts/setup31X.sh
+++ Utilities/scripts/setup31X.sh
@@ -8,10 +8,11 @@ eval `scramv1 ru -sh`
 cvs co RecoHI/Configuration
 
 cvs co RecoHI/HiTracking                        # Good tag?
-cvs co RecoPixelVertexing/PixelLowPtUtilities   # ?
+#cvs co RecoPixelVertexing/PixelLowPtUtilities   # ?
 
 cvs co RecoHI/HiJetAlgos
 cvs co RecoHI/HiEgammaAlgos
+cvs co RecoHI/HiCentralityAlgos
 
 cvs co GeneratorInterface/HydjetInterface
 cvs co -r embedding_v02 GeneratorInterface/PyquenInterface
@@ -21,8 +22,8 @@ cvs co SimDataFormats/HepMCProduct
 cvs co SimDataFormats/GeneratorProducts
 
 #cvs co -r $CMSSW_VERSION SimCalorimetry/HcalSimProducers
-#cat $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigiProducer.cc | replace "doZDC(true)" "doZDC(false)" | replace "e.getByLabel(\"mix\", zdcHitsName , zdccf)" "// e.getByLabel(\"mix\", zdcHitsName , zdccf)" | replace "colzdc(new MixCollection<PCaloHit>(zdccf.product()))" "colzdc(new MixCollection<PCaloHit>(new CrossingFrame<PCaloHit>))" | replace "theHitCorrection->fillChargeSums(*colzdc)" "// zdc correction" > tmp.cc
-#mv tmp.cc $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigiProducer.cc
+#cat $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc | replace "doZDC(true)" "doZDC(false)" | replace "e.getByLabel(\"mix\", zdcHitsName , zdccf)" "// e.getByLabel(\"mix\", zdcHitsName , zdccf)" | replace "colzdc(new MixCollection<PCaloHit>(zdccf.product()))" "colzdc(new MixCollection<PCaloHit>(new CrossingFrame<PCaloHit>))" | replace "theHitCorrection->fillChargeSums(*colzdc)" "// zdc correction" > tmp.cc
+#mv tmp.cc $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc
 
 cvs co UserCode/CmsHi
 mv UserCode/CmsHi .
