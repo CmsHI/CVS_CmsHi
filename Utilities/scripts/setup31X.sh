4f236a41102a2373d70f33ac6e6d510f3e52af66
edwenger <>
2009-06-19 08:39:40 +0000

    zdc digitization still needs to be turned off for embedding :(

--- Utilities/scripts/setup31X.sh
+++ Utilities/scripts/setup31X.sh
@@ -26,9 +26,9 @@ cvs co -r embedding_v02 GeneratorInterface/PyquenInterface
 cvs co SimDataFormats/HiGenData
 
 # Turn off ZDC digitization
-#cvs co -r $CMSSW_VERSION SimCalorimetry/HcalSimProducers
-#cat $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc | replace "doZDC(true)" "doZDC(false)" | replace "e.getByLabel(\"mix\", zdcHitsName , zdccf)" "// e.getByLabel(\"mix\", zdcHitsName , zdccf)" | replace "colzdc(new MixCollection<PCaloHit>(zdccf.product()))" "colzdc(new MixCollection<PCaloHit>(new CrossingFrame<PCaloHit>))" | replace "theHitCorrection->fillChargeSums(*colzdc)" "// zdc correction" > tmp.cc
-#mv tmp.cc $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc
+cvs co -r $CMSSW_VERSION SimCalorimetry/HcalSimProducers
+cat $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc | replace "doZDC(true)" "doZDC(false)" | replace "e.getByLabel(\"mix\", zdcHitsName , zdccf)" "// e.getByLabel(\"mix\", zdcHitsName , zdccf)" | replace "colzdc(new MixCollection<PCaloHit>(zdccf.product()))" "colzdc(new MixCollection<PCaloHit>(new CrossingFrame<PCaloHit>))" | replace "theHitCorrection->fillChargeSums(*colzdc)" "// zdc correction" > tmp.cc
+mv tmp.cc $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc
 
 cvs co UserCode/CmsHi
 mv UserCode/CmsHi .
