586aedef7cea4b5e89704c74658c3eac7879bc54
yilmaz <>
2009-06-22 08:55:33 +0000

    Ecal Gen-Level selection configuration for Heavy Ion simulations

---
+++ EgammaAnalysis/python/EcalGenTrigger_cff.py
@@ -0,0 +1,178 @@
+import FWCore.ParameterSet.Config as cms
+
+process.partontrig100 = cms.EDFilter("MCSingleParticleFilter",
+                                     moduleLabel = cms.untracked.string("signal"),
+                                     Status = cms.untracked.vint32(2,2,2,2,2,2,2,1),
+                                     MaxEta = cms.untracked.vdouble(3,3,3,3,3,3,3,3),
+                                     MinEta = cms.untracked.vdouble(-3,-3,-3,-3,-3,-3,-3,-3),
+                                     MinPt = cms.untracked.vdouble(38.5,38.5,38.5,38.5,38.5,38.5,38.5,38.5),
+                                     ParticleID = cms.untracked.vint32(1,2,3,4,5,6,21,22)
+                                     )
+
+
+
+process.ecaltrig100 = cms.EDFilter("MCSingleParticleFilter",
+                                   moduleLabel = cms.untracked.string("signal"),
+                                   Status = cms.untracked.vint32(2, 2, 2, 2, 2,
+                                                                 2, 1, 1, 2, 2,
+                                                                 1, 1, 1),
+                                   MaxEta = cms.untracked.vdouble(3, 3, 3, 3, 3,
+                                                                  3, 3, 3, 3, 3,
+                                                                  3, 3, 3),
+                                   MinEta = cms.untracked.vdouble(-3, -3, -3, -3, -3,
+                                                                  -3, -3, -3, -3, -3,
+                                                                  -3, -3, -3),
+                                   MinPt = cms.untracked.vdouble(36.125, 36.125, # eta
+                                                                 35.25, 35.25, # eta'
+                                                                 34.75, 34.75, # omega
+                                                                 32.25, 32.25, # pi
+                                                                 32.25,        # pi0
+                                                                 31.5,         # K0
+                                                                 38.75, 38.75, # e
+                                                                 71.625       # gamma
+                                                                 ),
+                                   
+                                   ParticleID = cms.untracked.vint32(221, -221, # eta
+                                                                     331, -331, # eta'
+                                                                     223, -223, # omega
+                                                                     211, -211, # pi
+                                                                     111,       # pi0
+                                                                     311,       # K0
+                                                                     11, -11,   # e
+                                                                     22         # gamma
+                                                                     )
+                                   )
+
+process.partontrig70 = cms.EDFilter("MCSingleParticleFilter",
+                                    moduleLabel = cms.untracked.string("signal"),
+                                    Status = cms.untracked.vint32(2,2,2,2,2,2,2,1),
+                                    MaxEta = cms.untracked.vdouble(3,3,3,3,3,3,3,3),
+                                    MinEta = cms.untracked.vdouble(-3,-3,-3,-3,-3,-3,-3,-3),
+                                    MinPt = cms.untracked.vdouble(38.5,38.5,38.5,38.5,38.5,38.5,38.5,38.5),
+                                    ParticleID = cms.untracked.vint32(1,2,3,4,5,6,21,22)
+                                    )
+
+process.ecaltrig70 = cms.EDFilter("MCSingleParticleFilter",
+                                  moduleLabel = cms.untracked.string("signal"),
+                                  Status = cms.untracked.vint32(2, 2, 2, 2, 2,
+                                                                2, 1, 1, 2, 2,
+                                                                1, 1, 1),
+                                  MaxEta = cms.untracked.vdouble(3, 3, 3, 3, 3,
+                                                                 3, 3, 3, 3, 3,
+                                                                 3, 3, 3),
+                                  MinEta = cms.untracked.vdouble(-3, -3, -3, -3, -3,
+                                                                 -3, -3, -3, -3, -3,
+                                                                 -3, -3, -3),
+                                  MinPt = cms.untracked.vdouble(17.5, 17.5, # eta
+                                                                16., 16., # eta'
+                                                                18., 18., # omega
+                                                                14.5, 14.5, # pi
+                                                                17.5,       # pi0
+                                                                15,       # K0
+                                                                17.5, 17.5,   # e
+                                                                38.5         # gamma
+                                                                ),
+                                  
+                                  ParticleID = cms.untracked.vint32(221, -221, # eta
+                                                                    331, -331, # eta'
+                                                                    223, -223, # omega
+                                                                    211, -211, # pi
+                                                                    111,       # pi0
+                                                                    311,       # K0
+                                                                    11, -11,   # e
+                                                                    22         # gamma
+                                                                    )
+                                  )
+
+
+
+process.partontrig40 = cms.EDFilter("MCSingleParticleFilter",
+                                    moduleLabel = cms.untracked.string("signal"),
+                                    Status = cms.untracked.vint32(2,2,2,2,2,2,2,1),
+                                    MaxEta = cms.untracked.vdouble(3,3,3,3,3,3,3,3),
+                                    MinEta = cms.untracked.vdouble(-3,-3,-3,-3,-3,-3,-3,-3),
+                                    MinPt = cms.untracked.vdouble(38.5,38.5,38.5,38.5,38.5,38.5,38.5,38.5),
+                                    ParticleID = cms.untracked.vint32(1,2,3,4,5,6,21,22)
+                                    )
+
+process.ecaltrig40 = cms.EDFilter("MCSingleParticleFilter",
+                                  moduleLabel = cms.untracked.string("signal"),
+                                  Status = cms.untracked.vint32(2, 2, 2, 2, 2,
+                                                                2, 1, 1, 2, 2,
+                                                                1, 1, 1),
+                                  MaxEta = cms.untracked.vdouble(3, 3, 3, 3, 3,
+                                                                 3, 3, 3, 3, 3,
+                                                                 3, 3, 3),
+                                  MinEta = cms.untracked.vdouble(-3, -3, -3, -3, -3,
+                                                                 -3, -3, -3, -3, -3,
+                                                                 -3, -3, -3),
+                                  MinPt = cms.untracked.vdouble(17.5, 17.5, # eta
+                                                                16., 16., # eta'
+                                                                18., 18., # omega
+                                                                14.5, 14.5, # pi
+                                                                17.5,       # pi0
+                                                                15,       # K0
+                                                                17.5, 17.5,   # e
+                                                                38.5         # gamma
+                                                                ),
+                                  
+                                  ParticleID = cms.untracked.vint32(221, -221, # eta
+                                                                    331, -331, # eta'
+                                                                    223, -223, # omega
+                                                                    211, -211, # pi
+                                                                    111,       # pi0
+                                                                    311,       # K0
+                                                                    11, -11,   # e
+                                                                    22         # gamma
+                                                                    )
+                                )
+
+
+process.partontrig20 = cms.EDFilter("MCSingleParticleFilter",
+                                    moduleLabel = cms.untracked.string("signal"),
+                                    Status = cms.untracked.vint32(2,2,2,2,2,2,2,1),
+                                    MaxEta = cms.untracked.vdouble(3,3,3,3,3,3,3,3),
+                                    MinEta = cms.untracked.vdouble(-3,-3,-3,-3,-3,-3,-3,-3),
+                                    MinPt = cms.untracked.vdouble(20,20,20,20,20,20,20,20),
+                                    ParticleID = cms.untracked.vint32(1,2,3,4,5,6,21,22)
+                                    )
+
+process.ecaltrig20 = cms.EDFilter("MCSingleParticleFilter",
+                                moduleLabel = cms.untracked.string("signal"),
+                                Status = cms.untracked.vint32(2, 2, 2, 2, 2,
+                                                              2, 1, 1, 2, 2,
+                                                              1, 1, 1),
+                                MaxEta = cms.untracked.vdouble(3, 3, 3, 3, 3,
+                                                               3, 3, 3, 3, 3,
+                                                               3, 3, 3),
+                                MinEta = cms.untracked.vdouble(-3, -3, -3, -3, -3,
+                                                               -3, -3, -3, -3, -3,
+                                                               -3, -3, -3),
+                                MinPt = cms.untracked.vdouble( 16.75,16.75, # eta
+                                                               10.5,10.5, # eta'
+                                                               16.375, 16.375, # omega
+                                                               13.25, 13.25, # pi
+                                                               12.5,       # pi0
+                                                               13.375,       # K0
+                                                               23.625,23.625,   # e
+                                                               21.625         # gamma
+                                                               ),
+                                
+                                ParticleID = cms.untracked.vint32(221, -221, # eta
+                                                                  331, -331, # eta'
+                                                                  223, -223, # omega
+                                                                  211, -211, # pi
+                                                                  111,       # pi0
+                                                                  311,       # K0
+                                                                  11, -11,   # e
+                                                                  22         # gamma
+                                                                  )
+                                )
+
+
+
+ecalGenTrigger100 = cms.Sequence(partontrig100*ecaltrig100)
+ecalGenTrigger70 = cms.Sequence(partontrig70*ecaltrig70)
+ecalGenTrigger40 = cms.Sequence(partontrig40*ecaltrig40)
+ecalGenTrigger20 = cms.Sequence(partontrig20*ecaltrig20)
+
