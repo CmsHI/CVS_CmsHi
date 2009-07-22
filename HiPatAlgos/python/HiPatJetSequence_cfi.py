6e26db0d17d9087a1b3f79026a5247368116b3e1
yilmaz <>
2009-07-22 16:45:56 +0000

    Jets fixed

--- HiPatAlgos/python/HiPatJetSequence_cfi.py
+++ HiPatAlgos/python/HiPatJetSequence_cfi.py
@@ -10,30 +10,29 @@ hiGenJetCleaner = cms.EDProducer('HiGenJetCleaner')
 #####################################################
 # Pat Jet Options
 
-process.allLayer1Jets.addBTagInfo = cms.bool(False)
-process.allLayer1Jets.addGenPartonMatch = cms.bool(False)
-process.allLayer1Jets.addAssociatedTracks = cms.bool(False)
-process.allLayer1Jets.addJetCharge = cms.bool(False)
-process.allLayer1Jets.addBTagInfo = cms.bool(False)
-process.allLayer1Jets.addDiscriminators = cms.bool(False)
-process.allLayer1Jets.addTagInfos = cms.bool(False)
-process.allLayer1Jets.getJetMCFlavour = cms.bool(False)
-
-process.allLayer1Jets.addGenJetMatch = cms.bool(True)
+allLayer1Jets.addBTagInfo = cms.bool(False)
+allLayer1Jets.addGenPartonMatch = cms.bool(False)
+allLayer1Jets.addAssociatedTracks = cms.bool(False)
+allLayer1Jets.addJetCharge = cms.bool(False)
+allLayer1Jets.addBTagInfo = cms.bool(False)
+allLayer1Jets.addDiscriminators = cms.bool(False)
+allLayer1Jets.addTagInfos = cms.bool(False)
+allLayer1Jets.getJetMCFlavour = cms.bool(False)
+allLayer1Jets.addGenJetMatch = cms.bool(True)
 
 #####################################################
 # Input Labels
 
-process.allLayer1Jets.jetSource = cms.InputTag("iterativeConePu5CaloJets")
+allLayer1Jets.jetSource = cms.InputTag("iterativeConePu5CaloJets")
 # (Placeholder for validated corrections)
-process.jetCorrFactors.jetSource = cms.InputTag("iterativeConePu5CaloJets")
-process.jetGenJetMatch.src = cms.InputTag("iterativeConePu5CaloJets")
-process.jetGenJetMatch.matched = cms.InputTag("hiGenJetCleaner")
+jetCorrFactors.jetSource = cms.InputTag("iterativeConePu5CaloJets")
+jetGenJetMatch.src = cms.InputTag("iterativeConePu5CaloJets")
+jetGenJetMatch.matched = cms.InputTag("hiGenJetCleaner")
 
-hiPatJetSequence = cms.Sequence(process.hiGenParticlesForJets *
-                                process.iterativeCone5HiGenJets*
-                                process.hiGenJetCleaner *
-                                process.jetGenJetMatch *
-                                process.jetCorrFactors *
-                                process.allLayer1Jets)
+hiPatJetSequence = cms.Sequence(hiGenParticlesForJets *
+                                iterativeCone5HiGenJets*
+                                hiGenJetCleaner *
+                                jetGenJetMatch *
+                                jetCorrFactors *
+                                allLayer1Jets)
 
