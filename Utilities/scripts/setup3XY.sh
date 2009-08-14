07ff9f574137a8148d4bef390bc52bfa1396697e
yilmaz <>
2009-08-14 13:50:33 +0000

    Moving slowly to official code

--- Utilities/scripts/setup3XY.sh
+++ Utilities/scripts/setup3XY.sh
@@ -17,6 +17,9 @@ cvs co -r embedding_v02 GeneratorInterface/PyquenInterface
 
 cvs co SimDataFormats/HiGenData
 
+cvs co SimGeneral/MixingModule
+cvs co SimGeneral/CrossingFrame
+
 # Turn off ZDC digitization
 checkOutVersion=`echo $CMSSW_VERSION | sed "s/_patch1//g"`
 ver=`echo $checkOutVersion | sed "s/CMSSW//g" | sed "s/_//g"`
@@ -25,6 +28,9 @@ cvs co -r $checkOutVersion SimCalorimetry/HcalSimProducers
 cat $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc | replace "doZDC(true)" "doZDC(false)" | replace "e.getByLabel(\"mix\", zdcHitsName , zdccf)" "// e.getByLabel(\"mix\", zdcHitsName , zdccf)" | replace "colzdc(new MixCollection<PCaloHit>(zdccf.product()))" "colzdc(new MixCollection<PCaloHit>(new CrossingFrame<PCaloHit>))" | replace "theHitCorrection->fillChargeSums(*colzdc)" "// zdc correction" > tmp.cc
 mv tmp.cc $CMSSW_BASE/src/SimCalorimetry/HcalSimProducers/src/HcalDigitizer.cc
 fi
+
+cvs co UserCode/yetkin/DataFormats
+mv UserCode/yetkin/DataFormats .
 
 cvs co UserCode/CmsHi
 mv UserCode/CmsHi .
