72fe847d84ddf30d88b18a482f5798f5eb2bb067
edwenger <>
2009-08-19 13:35:15 +0000

    changed input labels: signal -> hiSignal

--- Utilities/python/HiGenParticles_cfi.py
+++ Utilities/python/HiGenParticles_cfi.py
@@ -1,7 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 hiGenParticles = cms.EDProducer('HiGenParticleProducer',
-src = cms.vstring("signal","generator"),
-saveBarCodes = cms.untracked.bool(True),
-useCrossingFrame = cms.untracked.bool(False)
+src = cms.vstring("hiSignal","generator")
 )
