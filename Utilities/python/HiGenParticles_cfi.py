d5f39c3f0f14635c7cc30fc54b6cf672172be674
allfrey <>
2009-09-07 22:57:54 +0000

    adding default values for SaveBarcodes and useCrossingFrame

--- Utilities/python/HiGenParticles_cfi.py
+++ Utilities/python/HiGenParticles_cfi.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 hiGenParticles = cms.EDProducer('HiGenParticleProducer',
-src = cms.vstring("hiSignal","generator")
+src = cms.vstring("hiSignal","generator"),
+saveBarCodes = cms.untracked.bool(True),
+useCrossingFrame = cms.untracked.bool(False)
 )
