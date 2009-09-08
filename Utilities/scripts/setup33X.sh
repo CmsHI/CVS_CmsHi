dc981aa9735d6cdeda6cd7476a30983333daa3bd
yilmaz <>
2009-09-08 11:52:49 +0000

    PAT & Analysis stuff

--- Utilities/scripts/setup33X.sh
+++ Utilities/scripts/setup33X.sh
@@ -9,6 +9,12 @@ cvs co SimGeneral/MixingModule
 cvs co Configuration/Generator
 cvs co Configuration/StandardSequences
 
+# Stuff needed for PAT and Jet Analysis
+cvs co UserCode/CmsHi/JetAnalysis
+cvs co UserCode/CmsHi/HiPatAlgos
+mv UserCode/CmsHi .
+rm -r UserCode
+
 scramv1 b
 
 
