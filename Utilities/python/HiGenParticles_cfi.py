55dd6fb93407141a9763045dd17efbdbbe83fcaf
yilmaz <>
2009-09-14 13:21:50 +0000

    mix cff temporarily here until genparticles are in

--- Utilities/python/HiGenParticles_cfi.py
+++ Utilities/python/HiGenParticles_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 hiGenParticles = cms.EDProducer('GenParticleProducer',
-                                srcVector = cms.vstring("hiSignal","generator"),
+                                srcVector = cms.vstring("generator"),
                                 doSubEvent = cms.untracked.bool(True),
                                 useCrossingFrame = cms.untracked.bool(False),
                                 saveBarCodes = cms.untracked.bool(True)
