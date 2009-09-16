6ac0175109a5b1dc22f3a011c1c4ffad90a45555
edwenger <>
2009-09-16 10:14:20 +0000

    need HiMixing_EventContent to avoid error in cmsDriver

--- Utilities/scripts/setup33X_reco.sh
+++ Utilities/scripts/setup33X_reco.sh
@@ -7,10 +7,16 @@ eval `scramv1 ru -sh`
 
 cvs co Configuration/EventContent               # for EventContentHeavyIons
 cvs co Configuration/PyReleaseValidation        # for modified ConfigBuilder
+
 cvs co -r V00-00-05 RecoHI/Configuration        # for modified RecoHI_EventContent_cff
 cvs co -d Misc UserCode/edwenger/Misc
 mv Misc/EventContentHeavyIons_cff.py Configuration/EventContent/python
 mv Misc/ConfigBuilder.py Configuration/PyReleaseValidation/python
+
+#Need also HiMixing event content or cmsDriver breaks on python error
+cvs co SimGeneral/Configuration                 # for HiMixing_EventContent
+mv Misc/HiMixing_EventContent_cff.py SimGeneral/Configuration/python
+
 rm -r Misc
 
 scramv1 b
