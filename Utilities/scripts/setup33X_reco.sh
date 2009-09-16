03f5a78f44df7f4419523895f3534246f3edb81d
edwenger <>
2009-09-16 09:54:05 +0000

    reorganization or minimum patches

--- Utilities/scripts/setup33X_reco.sh
+++ Utilities/scripts/setup33X_reco.sh
@@ -8,14 +8,9 @@ eval `scramv1 ru -sh`
 cvs co Configuration/EventContent               # for EventContentHeavyIons
 cvs co Configuration/PyReleaseValidation        # for modified ConfigBuilder
 cvs co -r V00-00-05 RecoHI/Configuration        # for modified RecoHI_EventContent_cff
-cvs co UserCode/edwenger/Misc
-mv UserCode/edwenger/Misc/EventContentHeavyIons_cff.py Configuration/EventContent/python
-mv UserCode/edwenger/Misc/ConfigBuilder.py Configuration/PyReleaseValidation/python
-
-# HI Utilities and Examples
-cvs co UserCode/CmsHi/Utilities/python
-cvs co UserCode/CmsHi/Utilities/test
-mv UserCode/CmsHi .
-rm -r UserCode
+cvs co -d Misc UserCode/edwenger/Misc
+mv Misc/EventContentHeavyIons_cff.py Configuration/EventContent/python
+mv Misc/ConfigBuilder.py Configuration/PyReleaseValidation/python
+rm -r Misc
 
 scramv1 b
