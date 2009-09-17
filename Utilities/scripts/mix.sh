fcbe920003dab1978312e0bc7319b84795dd62ee
edwenger <>
2009-09-17 10:39:03 +0000

    new mixing sequences

---
+++ Utilities/scripts/mix.sh
@@ -0,0 +1,1 @@
+cmsDriver.py hiMix -s HISIGNAL:GammaJet_pt20,DIGI,L1,DIGI2RAW --scenario HeavyIons  --conditions FrontierConditions_GlobalTag,MC_31X_V8::All --datatier 'GEN-SIM-HISIGNAL-RAW' --eventcontent=RAWDEBUG --filein=/store/relval/CMSSW_3_3_0_pre3/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V8-v1/0015/DC571B73-43A1-DE11-BD0C-000423D98804.root -n 10 --no_exec
