1a2ff009274d2164d63bc91e7da494ea0a235024
edwenger <>
2009-09-22 13:41:31 +0000

    IOMC/RandomEngine for hiSignalLHCTransport

--- Utilities/scripts/setup33X.sh
+++ Utilities/scripts/setup33X.sh
@@ -11,6 +11,7 @@ cvs co -r V01-17-10 Configuration/StandardSequences          # for HiSignal_cff
 cvs co -r V11-01-00 PhysicsTools/HepMCCandAlgos/python       # for HiGenParticles_cfi
 cvs co Configuration/Generator                               # for hiSignal GEN configs
 addpkg Configuration/PyReleaseValidation                     # for modified ConfigBuilder
+cvs co -r CMSSW_3_3_0_pre4 IOMC/RandomEngine/python          # for hiSignalLHCTransport
 
 # HI Event Content
 cvs co -r V00-00-05 RecoHI/Configuration        # for modified RecoHI_EventContent_cff
@@ -18,8 +19,10 @@ cvs co -r V02-00-00 SimGeneral/Configuration    # for HiMixing_EventContent
 
 cvs co -d Misc UserCode/edwenger/Misc
 mv Misc/ConfigBuilder.py Configuration/PyReleaseValidation/python  # with HISIGNAL step
+mv Misc/cmsDriverOptions.py Configuration/PyReleaseValidation/python
 mv Misc/DigiHiMix_cff.py Configuration/StandardSequences/python    # hiTrackingParticles
 mv Misc/ReconstructionHeavyIons_cff.py Configuration/StandardSequences/python # use imported globalRecoPbPb sequence
+mv Misc/IOMC_cff.py IOMC/RandomEngine/python                       # with hiSignalLHCTransport for extended geometry
 rm -r Misc
 
 # Stuff needed for PAT and Jet Analysis
