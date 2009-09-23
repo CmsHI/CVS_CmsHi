d06cb55ae0e3433f0f4c48eccce23d2a6d76d2e2
edwenger <>
2009-09-23 11:25:15 +0000

    get latest cmsDriver stuff from PyReleaseValidation branch

--- Utilities/scripts/setup33X.sh
+++ Utilities/scripts/setup33X.sh
@@ -10,7 +10,7 @@ cvs co -r V05-00-01 SimGeneral/MixingModule/python           # for mixHiSignal_c
 cvs co -r V01-17-10 Configuration/StandardSequences          # for HiSignal_cff 
 cvs co -r V11-01-00 PhysicsTools/HepMCCandAlgos/python       # for HiGenParticles_cfi
 cvs co Configuration/Generator                               # for hiSignal GEN configs
-addpkg Configuration/PyReleaseValidation                     # for modified ConfigBuilder
+cvs co -r ap33X-branch Configuration/PyReleaseValidation     # for modified ConfigBuilder
 cvs co -r CMSSW_3_3_0_pre4 IOMC/RandomEngine/python          # for hiSignalLHCTransport
 
 # HI Event Content
@@ -19,8 +19,6 @@ cvs co -r V02-00-00 SimGeneral/Configuration    # for HiMixing_EventContent
 cvs co RecoHI/HiMuonAlgos/python   # for muonRecoPbPb sequence
 
 cvs co -d Misc UserCode/edwenger/Misc
-mv Misc/ConfigBuilder.py Configuration/PyReleaseValidation/python  # with HISIGNAL step
-mv Misc/cmsDriverOptions.py Configuration/PyReleaseValidation/python
 mv Misc/DigiHiMix_cff.py Configuration/StandardSequences/python    # hiTrackingParticles
 mv Misc/ReconstructionHeavyIons_cff.py Configuration/StandardSequences/python # use imported globalRecoPbPb sequence
 mv Misc/IOMC_cff.py IOMC/RandomEngine/python                       # with hiSignalLHCTransport for extended geometry
--- Utilities/scripts/setup33X_reco.sh
+++ Utilities/scripts/setup33X_reco.sh
@@ -5,14 +5,13 @@ export CVSROOT=:gserver:cmscvs.cern.ch:/cvs_server/repositories/CMSSW
 cd $CMSSW_BASE/src
 eval `scramv1 ru -sh`
 
-addpkg Configuration/PyReleaseValidation        # for modified ConfigBuilder
-cvs co  RecoHI/Configuration        # for modified RecoHI_EventContent_cff
-cvs co -r V02-00-00 SimGeneral/Configuration    # for HiMixing_EventContent
-addpkg Configuration/StandardSequences          # for modified ReconstructionHeavyIons_cff
-cvs co RecoHI/HiMuonAlgos/python    # for muonRecoPbPb sequence
+cvs co -r ap33X-branch Configuration/PyReleaseValidation # for modified ConfigBuilder
+cvs co  RecoHI/Configuration                             # for modified RecoHI_EventContent_cff
+cvs co -r V02-00-00 SimGeneral/Configuration             # for HiMixing_EventContent
+addpkg Configuration/StandardSequences                   # for modified ReconstructionHeavyIons_cff
+cvs co RecoHI/HiMuonAlgos/python                         # for muonRecoPbPb sequence
 
 cvs co -d Misc UserCode/edwenger/Misc
-mv Misc/ConfigBuilder.py Configuration/PyReleaseValidation/python # with HI event content
 mv Misc/ReconstructionHeavyIons_cff.py Configuration/StandardSequences/python # use imported globalRecoPbPb sequence
 rm -r Misc
 
