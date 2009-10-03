0b420db245509a72c13327a19b137c06a4e2f044
edwenger <>
2009-10-03 18:01:30 +0000

    minor fix. pre6 input files.

--- Utilities/scripts/mixGammaJet.sh
+++ Utilities/scripts/mixGammaJet.sh
@@ -1,5 +1,7 @@
-infile = "/store/relval/CMSSW_3_3_0_pre5/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V8-v1/0004/D2E41C64-41AB-DE11-890F-001D09F2932B.root"
-outfile = "gammajet20_MIX_RAW.root"
+#!/bin/sh
+
+infile="/store/relval/CMSSW_3_3_0_pre6/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0005/1CCBE44D-F1AF-DE11-A698-001D09F27003.root"
+outfile="gammajet20_MIX_RAW.root"
 
 cmsDriver.py Pyquen_GammaJet_pt20_4TeV_cfi.py -n 1 \
 -s GEN:hiSignal,SIM,DIGI,L1,DIGI2RAW  --scenario HeavyIons --himix \
--- Utilities/scripts/reco.sh
+++ Utilities/scripts/reco.sh
@@ -1,5 +1,7 @@
-infile = "/store/relval/CMSSW_3_3_0_pre5/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V8-v1/0004/D2E41C64-41AB-DE11-890F-001D09F2932B.root"
-outfile = "hydjetMB_RECO.root"
+#!/bin/sh
+
+infile="/store/relval/CMSSW_3_3_0_pre6/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0005/1CCBE44D-F1AF-DE11-A698-001D09F27003.root"
+outfile="hydjetMB_RECO.root"
 
 cmsDriver.py hiReco -n 1 \
 -s RAW2DIGI,RECO  --scenario HeavyIons \
