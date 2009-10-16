ac1bbdfba175995812a1824215c8dc99842339c8
edwenger <>
2009-10-16 15:39:34 +0000

    updated input files and global tags

--- TrackAnalysis/test/testVertexAnalyzer_cfg.py
+++ TrackAnalysis/test/testVertexAnalyzer_cfg.py
@@ -12,7 +12,7 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 #global tags for conditions data:
 #https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'MC_31X_V8::All'
+process.GlobalTag.globaltag = 'MC_31X_V9::All'
 
 ##################################################################################
 
@@ -22,8 +22,21 @@ options = VarParsing.VarParsing ('standard')
 # setup any defaults you want
 options.output = 'output_testVertex.root'
 options.secondaryOutput = 'edmFile_testVertex.root'
-#options.files= 'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/mc/hydjet_b4_310/HYDJET_MC31XV3_pt11_RECO_7.root'
-options.files = '/store/relval/CMSSW_3_2_5/RelValHydjetQ_B0_4TeV/GEN-SIM-RAW/MC_31X_V5-v1/0011/FC711C9E-4F8E-DE11-8DE0-003048D2C0F4.root'
+options.files = [
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/F8157400-F0B6-DE11-8940-001D09F29114.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/E82BBD98-F4B6-DE11-B138-001D09F2A690.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/E6F3BDBD-F9B6-DE11-BDAD-001D09F2437B.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/E6B3AB36-ECB6-DE11-A5A3-001D09F251FE.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/C6E6BA91-F6B6-DE11-8525-000423D987E0.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/A8564AC8-FCB6-DE11-8892-001D09F2924F.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/8CBE366A-09B7-DE11-9178-000423D33970.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/786C7243-00B7-DE11-8FF0-000423D94E70.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/6EA2BDAA-FEB6-DE11-88F8-001D09F24664.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/668FF650-03B7-DE11-9EC3-001D09F291D2.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/60C0481F-EFB6-DE11-985C-000423D98634.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/50A27EB5-E7B6-DE11-86F7-000423D9A2AE.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/3C01DE3C-F2B6-DE11-BB35-001D09F2AD4D.root',
+       '/store/relval/CMSSW_3_3_0/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V9-v1/0008/2E0345BE-06B7-DE11-9DC3-001D09F291D7.root' ]
 options.maxEvents = 1 
 
 # get and parse the command line arguments
