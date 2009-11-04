ffab0e75e9e1d50a049370f91b5deaafcbc538b1
kimy <>
2009-11-04 15:08:27 +0000

    modified the subevent mapping for 33x

--- PhotonAnalysis/interface/HiPhotonMCTruth.h
+++ PhotonAnalysis/interface/HiPhotonMCTruth.h
@@ -25,11 +25,9 @@
 class HiPhotonMCTruth
 {
  public:
-  HiPhotonMCTruth(edm::Handle<reco::GenParticleCollection> inputHandle, edm::Handle<edm::SubEventMap> subs);
-  bool indexMatch(const reco::Candidate &pp1);
+  HiPhotonMCTruth(edm::Handle<reco::GenParticleCollection> inputHandle);
   bool IsPrompt(const reco::GenParticle &pp);
-  bool IsPrompt(const reco::Candidate &pp);
-  bool IsIsolated(const reco::Candidate &pp);
+  bool IsIsolated(const reco::GenParticle &pp);
   
  private:
   HiMCGammaJetSignalDef mcisocut;
--- PhotonAnalysis/src/HiPhotonMCTruth.cc
+++ PhotonAnalysis/src/HiPhotonMCTruth.cc
@@ -16,88 +16,28 @@ using namespace reco;
 
 #define PI 3.141592653589793238462643383279502884197169399375105820974945
 
-HiPhotonMCTruth::HiPhotonMCTruth(edm::Handle<GenParticleCollection> inputHandle, edm::Handle<edm::SubEventMap> subs)
+HiPhotonMCTruth::HiPhotonMCTruth(edm::Handle<GenParticleCollection> inputHandle)
 {
   using namespace std;
 
   const GenParticleCollection *collection1 = inputHandle.product();
-  int maxindex = (int)collection1->size();
-  // cout << " Number of total Particles = " << maxindex << endl;
-  
-  vector<int> map;
-  
-  for(int i = 0; i < maxindex; i++)
-    {
-      int subEvtNum = (*subs)[GenParticleRef(inputHandle,i)];
-      //    cout << i<<"th particle belog to " << subEvtNum<<"th sub-event\"<<endl;              
-      map.push_back(subEvtNum);
-    }
-  cout << "map size = " << map.size() << endl;
- 
-  // HiMCGammaJetSignalDef mcisocut1(collection1 , map);
-  mcisocut = HiMCGammaJetSignalDef(collection1 , map);
+   mcisocut = HiMCGammaJetSignalDef(collection1);
   
 } 
 
-bool HiPhotonMCTruth::indexMatch(const reco::Candidate &pp)
-{
-  using namespace std;
-  using namespace edm;
-  using namespace reco;
-  // Check if a given particle is isolated.                                                                                                                               
 
-  if (mcisocut.getIndex(pp) == -1)
-    return false;
-  else
-    return true;
-}
-
-bool HiPhotonMCTruth::IsIsolated(const reco::Candidate &pp)
+bool HiPhotonMCTruth::IsIsolated(const reco::GenParticle &pp)
 {
   using namespace std;
   using namespace edm;
   using namespace reco;
    // Check if a given particle is isolated.
 
-  int ii = mcisocut.getIndex(pp);
-  if (ii==-1)
-    {
-      cout << " the index of candidate map number is -1.. error!!! " << endl;
-      //      if (pp.pdgId() ==22)
-      //cout << " and it is a photon!!! " << endl;
-      
-      return false;
-    }
-  return  mcisocut.IsIsolated(pp,ii);
+  return  mcisocut.IsIsolated(pp);
 }
 
 
 bool HiPhotonMCTruth::IsPrompt(const reco::GenParticle &pp)
-{
-  using namespace std;
-  if ( pp.pdgId() != 22)
-    return false;
-
-  if ( pp.mother() ==0 ) 
-    {
-      //      cout <<    "No mom for this particle.." << endl;
-      return false;
-    }
-  else 
-    {
-      if (pp.mother()->pdgId() == 22)
-	{
-	  cout << " found a prompt photon" << endl;
-	  return true;
-	}
-      else
-	return false;
-    }
-  
-  
-}
-
-bool HiPhotonMCTruth::IsPrompt(const reco::Candidate &pp)
 {
   using namespace std;
   if ( pp.pdgId() != 22)
