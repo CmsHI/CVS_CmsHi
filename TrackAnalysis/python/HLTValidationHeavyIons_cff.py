3c432b95206f6cffd6cd4a28d99516587e557441
edwenger <>
2010-03-10 14:15:04 +0000

    add photon hlt path

--- TrackAnalysis/python/HLTValidationHeavyIons_cff.py
+++ TrackAnalysis/python/HLTValidationHeavyIons_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 from CmsHi.TrackAnalysis.SingleJetValidationHI_cfi import *
-#from CmsHi.TrackAnalysis.EgammaValidationHI_cff import *
+from CmsHi.TrackAnalysis.EgammaValidationHI_cff import *
 
 hltValidationHI = cms.Sequence(hiSingleJetValidation
-                               #+ hiEgammaValidationSequence
+                               + hiEgammaValidationSequence
                                )
