16dde6abc53fbab48444a7fa2ff7358a11dc1b13
edwenger <>
2010-03-18 14:55:37 +0000

    modify ecal cluster labels for islandClustering algorithm (except pre-shower for which that is not yet implemented)

--- TrackAnalysis/python/globalValidationHeavyIons_cff.py
+++ TrackAnalysis/python/globalValidationHeavyIons_cff.py
@@ -10,6 +10,16 @@ hiTracks = 'hiSelectedTracks'
 PixelTrackingRecHitsValid.src = hiTracks
 StripTrackingRecHitsValid.trajectoryInput = hiTracks
 
+# change ecal labels for basic clusters and super-clusters
+egammaBasicClusterAnalyzer.barrelBasicClusterCollection = cms.InputTag("islandBasicClusters","islandBarrelBasicClusters")
+egammaBasicClusterAnalyzer.endcapBasicClusterCollection = cms.InputTag("islandBasicClusters","islandEndcapBasicClusters")
+egammaSuperClusterAnalyzer.barrelRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB")
+egammaSuperClusterAnalyzer.barrelRawSuperClusterCollection = cms.InputTag("islandSuperClusters","islandBarrelSuperClusters")
+egammaSuperClusterAnalyzer.barrelCorSuperClusterCollection = cms.InputTag("correctedIslandBarrelSuperClusters")
+egammaSuperClusterAnalyzer.endcapRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE")
+egammaSuperClusterAnalyzer.endcapRawSuperClusterCollection = cms.InputTag("islandSuperClusters","islandEndcapSuperClusters")
+egammaSuperClusterAnalyzer.endcapCorSuperClusterCollection = cms.InputTag("correctedIslandEndcapSuperClusters")
+#egammaSuperClusterAnalyzer.endcapPreSuperClusterCollection = cms.InputTag("islandEndcapSuperClustersWithPreshower") #to be implemented: only multi5x5 for now
 
 globalValidationHI = cms.Sequence(
     trackerHitsValidation      
