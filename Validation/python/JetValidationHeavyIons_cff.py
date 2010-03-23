0f5c0c2b931035cd770074f8edbf8daa849547df
edwenger <>
2010-03-23 10:38:25 +0000

    added genjet cleaner for improved matching

--- Validation/python/JetValidationHeavyIons_cff.py
+++ Validation/python/JetValidationHeavyIons_cff.py
@@ -1,11 +1,45 @@
 import FWCore.ParameterSet.Config as cms
 
-### this needs some genjet cleaning or improved matching criteria
-### to do proper matching in the heavy-ion environment
+### genjet cleaning for improved matching in HI environment
+
+iterativeCone5HiCleanedGenJets = cms.EDProducer('HiGenJetCleaner',
+  src    = cms.untracked.string('iterativeCone5HiGenJets'),
+  deltaR = cms.untracked.double(0.25),
+  ptCut  = cms.untracked.double(20),
+  createNewCollection = cms.untracked.bool(True),
+  fillDummyEntries = cms.untracked.bool(True)
+)
+
+iterativeCone7HiCleanedGenJets = cms.EDProducer('HiGenJetCleaner',
+  src    = cms.untracked.string('iterativeCone7HiGenJets'),
+  deltaR = cms.untracked.double(0.25),
+  ptCut  = cms.untracked.double(20),
+  createNewCollection = cms.untracked.bool(True),
+  fillDummyEntries = cms.untracked.bool(True)
+)
+
+ak5HiCleanedGenJets = cms.EDProducer('HiGenJetCleaner',
+  src    = cms.untracked.string('ak5HiGenJets'),
+  deltaR = cms.untracked.double(0.25),
+  ptCut  = cms.untracked.double(20),
+  createNewCollection = cms.untracked.bool(True),
+  fillDummyEntries = cms.untracked.bool(True)
+)
+
+ak7HiCleanedGenJets = cms.EDProducer('HiGenJetCleaner',
+  src    = cms.untracked.string('ak7HiGenJets'),
+  deltaR = cms.untracked.double(0.25),
+  ptCut  = cms.untracked.double(20),
+  createNewCollection = cms.untracked.bool(True),
+  fillDummyEntries = cms.untracked.bool(True)
+)
+
+### jet analyzer for two radii (0.5, 0.7) and three algorithms:
+### iterative cone with PU, anti-kt with PU, anti-kt with fastjet PU
 
 JetAnalyzerICPU5Calo = cms.EDAnalyzer("CaloJetTester",
     src = cms.InputTag("iterativeConePu5CaloJets"),
-    srcGen = cms.InputTag("iterativeCone5HiGenJets"),
+    srcGen = cms.InputTag("iterativeCone5HiCleanedGenJets"),
     genEnergyFractionThreshold = cms.double(0.05),
     genPtThreshold = cms.double(1.0),
     RThreshold = cms.double(0.3),
@@ -14,7 +48,7 @@ JetAnalyzerICPU5Calo = cms.EDAnalyzer("CaloJetTester",
 
 JetAnalyzerICPU7Calo = cms.EDAnalyzer("CaloJetTester",
     src = cms.InputTag("iterativeConePu7CaloJets"),
-    srcGen = cms.InputTag("iterativeCone7HiGenJets"), 
+    srcGen = cms.InputTag("iterativeCone7HiCleanedGenJets"), 
     genEnergyFractionThreshold = cms.double(0.05),
     genPtThreshold = cms.double(1.0),
     RThreshold = cms.double(0.3),
@@ -23,7 +57,7 @@ JetAnalyzerICPU7Calo = cms.EDAnalyzer("CaloJetTester",
 
 JetAnalyzerAkPU5Calo = cms.EDAnalyzer("CaloJetTester",
     src = cms.InputTag("akPu5CaloJets"),
-    srcGen = cms.InputTag("ak5HiGenJets"),  
+    srcGen = cms.InputTag("ak5HiCleanedGenJets"),  
     genEnergyFractionThreshold = cms.double(0.05),
     genPtThreshold = cms.double(1.0),
     RThreshold = cms.double(0.3),
@@ -32,7 +66,7 @@ JetAnalyzerAkPU5Calo = cms.EDAnalyzer("CaloJetTester",
 
 JetAnalyzerAkPU7Calo = cms.EDAnalyzer("CaloJetTester",
     src = cms.InputTag("akPu7CaloJets"),
-    srcGen = cms.InputTag("ak7HiGenJets"),       
+    srcGen = cms.InputTag("ak7HiCleanedGenJets"),       
     genEnergyFractionThreshold = cms.double(0.05),
     genPtThreshold = cms.double(1.0),
     RThreshold = cms.double(0.3),
@@ -41,7 +75,7 @@ JetAnalyzerAkPU7Calo = cms.EDAnalyzer("CaloJetTester",
 
 JetAnalyzerAkFastPU5Calo = cms.EDAnalyzer("CaloJetTester",
     src = cms.InputTag("akFastPu5CaloJets"),
-    srcGen = cms.InputTag("ak5HiGenJets"),  
+    srcGen = cms.InputTag("ak5HiCleanedGenJets"),  
     genEnergyFractionThreshold = cms.double(0.05),
     genPtThreshold = cms.double(1.0),
     RThreshold = cms.double(0.3),
@@ -50,17 +84,16 @@ JetAnalyzerAkFastPU5Calo = cms.EDAnalyzer("CaloJetTester",
 
 JetAnalyzerAkFastPU7Calo = cms.EDAnalyzer("CaloJetTester",
     src = cms.InputTag("akFastPu7CaloJets"),
-    srcGen = cms.InputTag("ak7HiGenJets"),       
+    srcGen = cms.InputTag("ak7HiCleanedGenJets"),       
     genEnergyFractionThreshold = cms.double(0.05),
     genPtThreshold = cms.double(1.0),
     RThreshold = cms.double(0.3),
     reverseEnergyFractionThreshold = cms.double(0.5)
 )
 
-hiJetValidation = cms.Sequence(JetAnalyzerICPU5Calo
-                               #*JetAnalyzerICPU7Calo
-                               #*JetAnalyzerAkPU5Calo
-                               #*JetAnalyzerAkPU7Calo
-                               #*JetAnalyzerAkFastPU5Calo
-                               #*JetAnalyzerAkFastPU7Calo
-                               )
+hiJetValidation = cms.Sequence(
+    (iterativeCone5HiCleanedGenJets * JetAnalyzerICPU5Calo) 
+    #+ (iterativeCone7HiCleanedGenJets * JetAnalyzerICPU7Calo)
+    #+ (ak5HiCleanedGenJets * JetAnalyzerAkPU5Calo * JetAnalyzerAkFastPU5Calo
+    #+ (ak7HiCleanedGenJets*JetAnalyzerAkPU7Calo *JetAnalyzerAkFastPU7Calo)
+    )
