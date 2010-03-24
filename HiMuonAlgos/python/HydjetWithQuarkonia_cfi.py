0ebbb25bcbe99234c8270861404512fd6d5f1aac
silvest <>
2010-03-24 22:01:30 +0000

    adding quarkonia and weak boson to hydjet

---
+++ HiMuonAlgos/python/HydjetWithQuarkonia_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Hydjet_Quenched_MinBias_2760GeV_cfi import *
+myPars = cms.vstring('pythiaBottomoniumNRQCD',
+                     'pythiaCharmoniumNRQCD',
+                     'pythiaWeakBosons')
+generator.PythiaParameters.parameterSets.extend(myPars)
