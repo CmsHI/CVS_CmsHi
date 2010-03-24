6426cb1f84694966c64a24e8e051c0094c771621
silvest <>
2010-03-24 23:13:01 +0000

    rm bosons

--- HiMuonAlgos/python/HydjetWithQuarkonia_cfi.py
+++ HiMuonAlgos/python/HydjetWithQuarkonia_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Hydjet_Quenched_MinBias_2760GeV_cfi import *
 myPars = cms.vstring('pythiaBottomoniumNRQCD',
-                     'pythiaCharmoniumNRQCD',
-                     'pythiaWeakBosons')
+                     'pythiaCharmoniumNRQCD')
 generator.PythiaParameters.parameterSets.extend(myPars)
