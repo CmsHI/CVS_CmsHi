f2d8b0c4e2c6fdd3c77f359225a7e15458ade857
silvest <>
2010-04-06 14:14:44 +0000

    adding pythiaQuarkoniaSettings

--- HiMuonAlgos/python/HydjetWithQuarkoniaWeakBosons_cfi.py
+++ HiMuonAlgos/python/HydjetWithQuarkoniaWeakBosons_cfi.py
@@ -1,7 +1,9 @@
 # example cmsDriver.py CmsHi/HiMuonAlgos/HydjetWithQuarkoniaWeakBosons_cfi -s GEN,SIM,DIGI,L1,DIGI2RAW -n 5 --geometry DB --conditions auto:mc --datatier 'GEN-SIM-RAW' --eventcontent RAWDEBUG --scenario HeavyIons --no_exec
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Hydjet_Quenched_MinBias_2760GeV_cfi import *
-myPars = cms.vstring('pythiaBottomoniumNRQCD',
+myPars = cms.vstring(#'pythiaJets','pythiaZjets',
+                     'pythiaBottomoniumNRQCD',
                      'pythiaCharmoniumNRQCD',
+                     'pythiaQuarkoniaSettings',
                      'pythiaWeakBosons')
 generator.PythiaParameters.parameterSets.extend(myPars)
--- HiMuonAlgos/python/HydjetWithQuarkoniaWeakBosons_pthat4_cfi.py
+++ HiMuonAlgos/python/HydjetWithQuarkoniaWeakBosons_pthat4_cfi.py
@@ -1,8 +1,10 @@
 # example cmsDriver.py CmsHi/HiMuonAlgos/HydjetWithQuarkoniaWeakBosons_pthat4_cfi -s GEN,SIM,DIGI,L1,DIGI2RAW -n 5 --geometry DB --conditions auto:mc --datatier 'GEN-SIM-RAW' --eventcontent RAWDEBUG --scenario HeavyIons --no_exec
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Hydjet_Quenched_MinBias_2760GeV_cfi import *
-myPars = cms.vstring('pythiaBottomoniumNRQCD',
+myPars = cms.vstring(#'pythiaJets','pythiaZjets'
+                     'pythiaBottomoniumNRQCD',
                      'pythiaCharmoniumNRQCD',
+                     'pythiaQuarkoniaSettings',
                      'pythiaWeakBosons')
 generator.PythiaParameters.parameterSets.extend(myPars)
 generator.PythiaParameters.ppDefault = cms.vstring('MSEL=1   ! QCD hight pT processes',
