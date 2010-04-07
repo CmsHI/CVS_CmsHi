01490f92e1b430553bed1ca6ebb1de3e6459d9ca
silvest <>
2010-04-07 13:57:57 +0000

    with jets

--- HiMuonAlgos/python/HydjetWithQuarkonia_cfi.py
+++ HiMuonAlgos/python/HydjetWithQuarkonia_cfi.py
@@ -1,7 +1,7 @@
 #example cmsDriver.py CmsHi/HiMuonAlgos/HydjetWithQuarkonia_cfi -s GEN,SIM,DIGI,L1,DIGI2RAW -n 5 --geometry DB --conditions auto:mc --datatier 'GEN-SIM-RAW' --eventcontent RAWDEBUG --scenario HeavyIons --no_exec
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Hydjet_Quenched_MinBias_2760GeV_cfi import *
-myPars = cms.vstring(#'pythiaJets','pythiaZjets',
+myPars = cms.vstring('pythiaJets',
                      'pythiaBottomoniumNRQCD',
                      'pythiaCharmoniumNRQCD',
                      'pythiaQuarkoniaSettings')
