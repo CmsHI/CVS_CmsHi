4809ee5b4f7456dc96fc1eeb6146805dd328f2e5
sungho <>
2010-04-26 13:09:13 +0000

    primary charged sim track included

--- TrackAnalysis/python/findableSimTracks_cfi.py
+++ TrackAnalysis/python/findableSimTracks_cfi.py
@@ -17,3 +17,20 @@ findableSimTracks = cms.EDFilter("HitPixelLayersTPSelection",
 )
 
 
+primaryChgSimTracks = cms.EDFilter("HitPixelLayersTPSelection",
+    src = cms.InputTag("mergedtruth","MergedTrackTruth"),
+          tripletSeedOnly = cms.bool(False),
+          chargedOnly = cms.bool(True),
+          signalOnly = cms.bool(False),
+          primaryOnly = cms.bool(True),
+          tpStatusBased = cms.bool(True),
+          ptMin = cms.double(0.1),
+          minHit = cms.int32(3),
+          minRapidity = cms.double(-2.5),
+          maxRapidity = cms.double(2.5),
+          tip = cms.double(3.5),
+          lip = cms.double(30.0),
+          pdgId = cms.vint32()
+)
+
+
