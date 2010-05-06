0f8eee5c90928b63484e13c1b142ced20b556333
yilmaz <>
2010-05-06 12:27:24 +0000

    set up for 3_6

---
+++ JetAnalysis/scripts/setup36X.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd $CMSSW_BASE/src
+
+cvs co PhysicsTools/PatAlgos
+cvs co DataFormats/PatCandidates
+cvs co RecoHI/HiJetAlgos
+cvs co HeavyIonsAnalysis/Configuration
+cvs co GeneratorInterface/HiGenCommon
+
+# JRA stuff
+cvs co -r V00-07-04 JetMETAnalysis/JetUtilities
+cvs co -r V00-08-06 JetMETAnalysis/JetAnalyzers
+
+# JetMET stuff
+cvs co -r jetCorrections_3_6_0_retrofit CondFormats/JetMETObjects
+cvs co -r jetCorrections_3_6_0_retrofit JetMETCorrections/Configuration
+cvs co -r jetCorrections_3_6_0_retrofit JetMETCorrections/Modules
+mv CondFormats/JetMETObjects/data/L5Flavor_IC5Calo.txt CondFormats/JetMETObjects/data/L5Flavor_IC5.txt 
+
+cvs co UserCode/CmsHi/JetAnalysis
+mv UserCode/CmsHi/JetAnalysis/bin/jet_response_analyzer_x.cc JetMETAnalysis/JetAnalyzers/bin/
+scram b
+`pwd`
+
+
