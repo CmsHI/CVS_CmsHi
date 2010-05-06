8a46ffbec8d19d6b9f7709df3abeb9ce0550f699
yilmaz <>
2010-05-06 09:41:38 +0000

    Fake jet producer

--- JetAnalysis/scripts/setup.sh
+++ JetAnalysis/scripts/setup.sh
@@ -3,6 +3,10 @@
 cd $CMSSW_BASE/src
 cvs co -r V00-07-04 JetMETAnalysis/JetUtilities
 cvs co -r V00-08-06 JetMETAnalysis/JetAnalyzers
+cvs co PhysicsTools/PatAlgos
+cvs co RecoHI/HiJetAlgos
+cvs co HeavyIonsAnalysis/Configuration
+cvs co GeneratorInterface/HiGenCommon
 cvs co -r jetCorrections_3_6_0_retrofit CondFormats/JetMETObjects
 mv CondFormats/JetMETObjects/data/L5Flavor_IC5Calo.txt CondFormats/JetMETObjects/data/L5Flavor_IC5.txt 
 
