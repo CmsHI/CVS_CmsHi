c5c3a2fea75f1a540808731326beb3b8516df789
kumarv <>
2010-05-14 10:33:36 +0000

    *** empty log message ***

---
+++ Z0Ana/ZToMuMu/python/ZMuMuEventContent_cff.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+ZMuMuContent = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+    #'keep *',
+       
+    'keep *_generator_*_*',
+    'keep *_genParticles_*_*',
+    'keep *_hiCentrality_*_*',
+    'keep *_hiEvtPlane_*_*',
+    'keep patMuons_*_*_*',
+    'keep recoMuons_muons_*_*',
+    'keep *_dimuonsGlobal_*_*',
+    'keep *_dimuonsGlobalSTA_*_*',
+    'keep *_dimuonsSTA_*_*',
+    'keep *_GenDimuons_*_*',
+    'keep *_Dimuons_*_*'
+    
+    )
+    )
+
+
+
+
