ad5f170ad9c7ad183cf0077602933aae625671c7
yilmaz <>
2010-05-17 16:02:37 +0000

    no jpt

--- JetAnalysis/scripts/setup36X.sh
+++ JetAnalysis/scripts/setup36X.sh
@@ -3,7 +3,7 @@
 cd $CMSSW_BASE/src
 
 cvs co -r V07-13-19 PhysicsTools/PatAlgos # B3_6_X ?
-cvs co -r V09-05-17 DataFormats/PatCandidates # B3_6_X ?
+cvs co -r V05-09-14-00  DataFormats/PatCandidates # B3_6_X ?
 cvs co -r V03-01-14 RecoMET/METAlgorithms/python
 
 cvs co -r V00-00-09 RecoHI/HiJetAlgos
