7ad265873153f7fe4f03b91f10213a96bd172bec
yilmaz <>
2010-05-17 15:42:53 +0000

    check out the met algorithms

--- JetAnalysis/scripts/setup36X.sh
+++ JetAnalysis/scripts/setup36X.sh
@@ -4,6 +4,8 @@ cd $CMSSW_BASE/src
 
 cvs co -r V07-13-19 PhysicsTools/PatAlgos # B3_6_X ?
 cvs co -r V09-05-17 DataFormats/PatCandidates # B3_6_X ?
+cvs co -r V03-01-14 RecoMET/METAlgorithms/python
+
 cvs co -r V00-00-09 RecoHI/HiJetAlgos
 cvs co HeavyIonsAnalysis/Configuration
 cvs co GeneratorInterface/HiGenCommon
