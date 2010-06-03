d95b1a1ae16f7b267d487b339803046a6a970d5b
yilmaz <>
2010-06-03 09:24:20 +0000

    setup updated to 3_7_0

--- JetAnalysis/scripts/setup.sh
+++ JetAnalysis/scripts/setup.sh
@@ -2,18 +2,22 @@
 
 cd $CMSSW_BASE/src
 cvs co RecoHI/HiCentralityAlgos/data
+cvs co HeavyIonsAnalysis/Configuration
+
+# JRA stuff
 cvs co -r V00-07-04 JetMETAnalysis/JetUtilities
 cvs co -r V00-08-06 JetMETAnalysis/JetAnalyzers
-cvs co RecoHI/HiJetAlgos
-cvs co HeavyIonsAnalysis/Configuration
-cvs co GeneratorInterface/HiGenCommon
+
+# JetMET stuff
 cvs co -r jetCorrections_3_6_0_retrofit CondFormats/JetMETObjects
+cvs co -r jetCorrections_3_6_0_retrofit JetMETCorrections/Configuration
+cvs co -r jetCorrections_3_6_0_retrofit JetMETCorrections/Modules
 mv CondFormats/JetMETObjects/data/L5Flavor_IC5Calo.txt CondFormats/JetMETObjects/data/L5Flavor_IC5.txt 
 
 cvs co UserCode/CmsHi/JetAnalysis
 mv UserCode/CmsHi/JetAnalysis/bin/jet_response_analyzer_x.cc JetMETAnalysis/JetAnalyzers/bin/
 rm JetMETAnalysis/JetAnalyzers/python/JPTReconstruction_cff.py
 scram b
-`pwd`
+pwd
 
 
