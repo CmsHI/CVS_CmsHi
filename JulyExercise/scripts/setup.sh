e204befbef04624e10ad96f9c52a2623f28a27ba
yilmaz <>
2010-07-05 21:55:48 +0000

    some stuff removed

--- JulyExercise/scripts/setup.sh
+++ JulyExercise/scripts/setup.sh
@@ -15,16 +15,6 @@ cvs co -r jetCorrections_3_6_0_retrofit JetMETCorrections/Configuration
 cvs co -r jetCorrections_3_6_0_retrofit JetMETCorrections/Modules
 mv CondFormats/JetMETObjects/data/L5Flavor_IC5Calo.txt CondFormats/JetMETObjects/data/L5Flavor_IC5.txt 
 
-cvs co -d CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
-mv CmsHi/JetAnalysis/bin/jet_response_analyzer_x.cc JetMETAnalysis/JetAnalyzers/bin/
-rm JetMETAnalysis/JetAnalyzers/python/JPTReconstruction_cff.py
-
-mv CmsHi/JetAnalysis/python/jetMETCorrections_cff.py PhysicsTools/PatAlgos/python/recoLayer0/jetMETCorrections_cff.py
-
-
-
-
-
 scram b
 
 
