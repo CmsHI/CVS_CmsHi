37d03bdf3384d6fc3add4ed143ede1131e291c15
yilmaz <>
2010-07-06 12:59:55 +0000

    some temporary customisation in event content

--- JulyExercise/python/highPtAnalysisSkim.py
+++ JulyExercise/python/highPtAnalysisSkim.py
@@ -24,6 +24,8 @@ process.output = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string("jetAnaSkim.root")
     )
 
+process.output.outputCommands.extend( "keep recoPhotons_*_*_*" )
+
 process.prod = cms.Path(process.allTracks +
     #process.heavyIon +
     process.makeHeavyIonJets
