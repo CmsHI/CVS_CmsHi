b4a57f25ccfc650b5fa4cc5b1835d7edfb988619
yilmaz <>
2010-07-07 17:09:49 +0000

    new vertex for sim

--- JulyExercise/python/highPtAnalysisSkim.py
+++ JulyExercise/python/highPtAnalysisSkim.py
@@ -3,12 +3,28 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process('PAT')
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring("reco.root")
-    )
+                            fileNames = cms.untracked.vstring("rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_7_0/RelValHydjetQ_MinBias_2760GeV/GEN-SIM-RECO/MC_37Y_V4-v1/0025/1E466625-6169-DF11-A543-002618943829.root"),
+                            inputCommands = cms.untracked.vstring("keep *_*_*_*",
+                                                                  "drop *_iterativeConePu5CaloJets_*_*"
+                                                                  ),
+                            dropDescendantsOfDroppedBranches = cms.untracked.bool(False)
+                            )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(2)
     )
+
+process.load('Configuration/StandardSequences/GeometryExtended_cff')
+
+process.load('Configuration/StandardSequences/ReconstructionHeavyIons_cff')
+
+process.hiCentrality.produceHFhits = False
+process.hiCentrality.produceHFtowers = False
+process.hiCentrality.produceEcalhits = False
+process.hiCentrality.produceBasicClusters = False
+process.hiCentrality.produceZDChits = False
+process.hiCentrality.produceETmidRapidity = True
+
 
 process.load('PhysicsTools.PatAlgos.patHeavyIonSequences_cff')
 from PhysicsTools.PatAlgos.tools.heavyIonTools import *
@@ -21,13 +37,22 @@ process.load("HeavyIonsAnalysis.Configuration.analysisEventContent_cff")
 
 process.output = cms.OutputModule("PoolOutputModule",
     process.jetTrkSkimContent,
-    fileName = cms.untracked.string("jetAnaSkim.root")
+    fileName = cms.untracked.string("allenrichedNZS.root")
     )
 
-process.output.outputCommands.extend( "keep recoPhotons_*_*_*" )
+process.output.outputCommands.extend([
+    "keep recoVertexs_hiSelectedVertex__RECO",
+#    "keep *_hiSelectedTracks_*_*",
+    "keep recoTracks_hiSelectedTracks__RECO",
+    "keep recoPhotons_*_*_*" ,
+    "keep edmTriggerResults_TriggerResults__HISIGNAL2" ,
+    "keep triggerTriggerEvent_hltTriggerSummaryAOD__HISIGNAL2"
+    ])
 
-process.prod = cms.Path(process.allTracks +
-    #process.heavyIon +
+process.prod = cms.Path(
+    # process.allTracks +
+    process.iterativeConePu5CaloJets +
+    process.hiCentrality +
     process.makeHeavyIonJets
     )
 
