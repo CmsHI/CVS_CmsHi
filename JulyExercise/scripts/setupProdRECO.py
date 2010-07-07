623ead862dd681bcd0dd325df8290d15416b0d6a
yilmaz <>
2010-07-07 09:51:56 +0000

    setup for RECO

--- JulyExercise/scripts/setupProdRECO.py
+++ JulyExercise/scripts/setupProdRECO.py
@@ -2,6 +2,7 @@
 
 cvs co -r V04-00-01 RecoJets/JetAlgorithms
 cvs co -r V05-01-04 RecoJets/JetProducers
+cvs co DataFormats/HeavyIonEvent
 cvs co RecoHI/HiJetAlgos
 cvs co RecoHI/HiCentralityAlgos
 
