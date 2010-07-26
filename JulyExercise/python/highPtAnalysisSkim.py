a175fd65ac46bd45d1e05a593f72c41fd2c55ff5
nart <>
2010-07-26 11:31:16 +0000

    update

--- JulyExercise/python/highPtAnalysisSkim.py
+++ JulyExercise/python/highPtAnalysisSkim.py
@@ -3,25 +3,31 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('PAT')
 
+
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring("rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_7_0/RelValHydjetQ_MinBias_2760GeV/GEN-SIM-RECO/MC_37Y_V4-v1/0025/1E466625-6169-DF11-A543-002618943829.root"),
+                            fileNames = cms.untracked.vstring("dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/user/kimy/MinimumBiasHI/Spring10-JulyAnalysisExercise_MC_37Y_V4-HardEnriched-GEN-SIM-RECO/6a7752772a3d4259db7cafda4079c785/hiRecoJEX_RAW2DIGI_RECO_9_1_062.root"),
+
                             inputCommands = cms.untracked.vstring("keep *_*_*_*",
                                                                   "drop *_iterativeConePu5CaloJets_*_*"
                                                                   ),
                             dropDescendantsOfDroppedBranches = cms.untracked.bool(False)
+                            
                             )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(2)
     )
 
-process.load('Configuration/StandardSequences/GeometryExtended_cff')
 
+process.load('Configuration/StandardSequences/GeometryExtended_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration/StandardSequences/MagneticField_38T_cff')
 process.GlobalTag.globaltag = 'MC_37Y_V5::All'
 
+
 process.load('Configuration/StandardSequences/ReconstructionHeavyIons_cff')
+process.load('PhysicsTools.PatAlgos.patHeavyIonSequences_cff')
+# process.load('RecoHI.HiJetAlgos.HiGenCleaner_cff')
 
 process.hiCentrality.produceHFhits = False
 process.hiCentrality.produceHFtowers = False
@@ -31,19 +37,21 @@ process.hiCentrality.produceZDChits = False
 process.hiCentrality.produceETmidRapidity = True
 process.hiCentrality.producePixelhits = True
 
-process.load('PhysicsTools.PatAlgos.patHeavyIonSequences_cff')
+
 from PhysicsTools.PatAlgos.tools.heavyIonTools import *
 configureHeavyIons(process)
 
-from PhysicsTools.PatAlgos.tools.jetTools import *
 
 process.load("HeavyIonsAnalysis.Configuration.analysisProducers_cff")
 process.load("HeavyIonsAnalysis.Configuration.analysisEventContent_cff")
 
+
 process.output = cms.OutputModule("PoolOutputModule",
                                   process.jetTrkSkimContent,
-                                      fileName = cms.untracked.string("allenrichedNZS.root")
+                                      fileName = cms.untracked.string("output.root")
                                   )
+
+
 
 process.output.outputCommands.extend([
     "keep recoVertexs_hiSelectedVertex__RECO",
@@ -60,10 +68,11 @@ process.prod = cms.Path(
     process.ak5CaloJets +
     process.hiCentrality +
     process.makeHeavyIonJets
+    
     )
-
-from CmsHi.JulyExercise.DisableMC_cff import *
-disableMC(process)
+ 
+#from CmsHi.JulyExercise.DisableMC_cff import *
+#disableMC(process)
 
 process.out_step = cms.EndPath(process.output)
 
