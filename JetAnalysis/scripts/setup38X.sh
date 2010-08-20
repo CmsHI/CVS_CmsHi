c1ad0f771692c1b47ed10014958a7b4c7e3b7b89
frankma <>
2010-08-20 18:43:06 +0000

    update PatAlgos tag. Also some cleanup

--- JetAnalysis/scripts/setup38X.sh
+++ JetAnalysis/scripts/setup38X.sh
@@ -1,18 +1,19 @@
 #!/bin/sh                                                                                                                                                   
 cvs co -d CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
 
+cvs co RecoHI/Configuration
 cvs co RecoHI/HiCentralityAlgos
+cvs co RecoHI/HiEvtPlaneAlgos
+cvs co RecoHI/HiJetAlgos
 cvs co RecoHI/HiMuonAlgos
-cvs co RecoHI/HiCentralityAlgos/data
 
 cvs co RecoJets/JetAlgorithms
 cvs co RecoJets/JetProducers
 
 cvs co HeavyIonsAnalysis/Configuration
 cvs co DataFormats/HeavyIonEvent
+cvs co SimDataFormats/HiGenData
 
-cvs co -r $CMSSW_VERSION PhysicsTools/PatAlgos/python
-cvs co CondFormats/JetMETObjects/data
-mv CondFormats/JetMETObjects/data/L5Flavor_IC5Calo.txt CondFormats/JetMETObjects/data/L5Flavor_IC5.txt
+cvs co -r V08-00-24 PhysicsTools/PatAlgos
 
 scram b
