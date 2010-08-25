2898e923e39d564d19c7d53d2cfe750494ff4001
frankma <>
2010-08-25 22:51:56 +0000

    update for 3_8_2. Also slight cleanup

--- JetAnalysis/scripts/setup38X.sh
+++ JetAnalysis/scripts/setup38X.sh
@@ -1,7 +1,6 @@
 #!/bin/sh                                                                                                                                                   
 cvs co -d CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
 
-cvs co RecoHI/Configuration
 cvs co RecoHI/HiCentralityAlgos
 cvs co RecoHI/HiEvtPlaneAlgos
 cvs co RecoHI/HiJetAlgos
@@ -14,7 +13,5 @@ cvs co RecoJets/JetProducers
 cvs co HeavyIonsAnalysis/Configuration
 cvs co DataFormats/HeavyIonEvent
 cvs co SimDataFormats/HiGenData
-
-cvs co -r V08-00-24 PhysicsTools/PatAlgos
 
 scram b
