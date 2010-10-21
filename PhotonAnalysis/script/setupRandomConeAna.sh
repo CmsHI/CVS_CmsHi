4fff70c0576b8ae2b3593c1985eef57d8bf522db
kimy <>
2010-10-21 16:18:18 +0000

    removed the tag...

--- PhotonAnalysis/script/setupRandomConeAna.sh
+++ PhotonAnalysis/script/setupRandomConeAna.sh
@@ -1,5 +1,5 @@
 cd $CMSSW_BASE/src
 
-cvs co -r Oct-20-2010 -d RandomConeAna UserCode/EYKim/photonStudy/RandomConeAna
+cvs co -d RandomConeAna UserCode/EYKim/photonStudy/RandomConeAna
 
 # don't need the Supercluster seed patch anymore. 
