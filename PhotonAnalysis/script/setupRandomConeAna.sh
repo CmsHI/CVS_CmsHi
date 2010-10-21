0383bfbc95f9d832ca0245647f2977801428c508
kimy <>
2010-10-21 12:39:36 +0000

    don't need the Egamma patch anymore

--- PhotonAnalysis/script/setupRandomConeAna.sh
+++ PhotonAnalysis/script/setupRandomConeAna.sh
@@ -1,16 +1,5 @@
 cd $CMSSW_BASE/src
 
-cvs co -d RandomConeAna UserCode/EYKim/photonStudy/RandomConeAna
-addpkg RecoEgamma/PhotonIdentification
-addpkg RecoEgamma/EgammaIsolationAlgos
+cvs co -r Oct-20-2010 -d RandomConeAna UserCode/EYKim/photonStudy/RandomConeAna
 
-# apply Yongsun's patch (ignore version dependence for the moment)
-mv RandomConeAna/patches/PhotonIsolationCalculator.cc  $CMSSW_BASE/src/RecoEgamma/PhotonIdentification/src
-mv RandomConeAna/patches/PhotonIsolationCalculator.h   $CMSSW_BASE/src/RecoEgamma/PhotonIdentification/interface
-mv RandomConeAna/patches/EgammaRecHitIsolation.cc      $CMSSW_BASE/src/RecoEgamma/EgammaIsolationAlgos/src
-mv RandomConeAna/patches/EgammaRecHitIsolation.h       $CMSSW_BASE/src/RecoEgamma/EgammaIsolationAlgos/interface
-mv RandomConeAna/patches/isolationCalculator_cfi.py    $CMSSW_BASE/src/RecoEgamma/PhotonIdentification/python
-mv RandomConeAna/patches/EgammaEcalRecHitIsolationProducer.h   $CMSSW_BASE/src/RecoEgamma/EgammaIsolationAlgos/plugins
-mv RandomConeAna/patches/EgammaEcalRecHitIsolationProducer.cc  $CMSSW_BASE/src/RecoEgamma/EgammaIsolationAlgos/plugins
-
-rm RandomConAna/patches -rf
+# don't need the Supercluster seed patch anymore. 
