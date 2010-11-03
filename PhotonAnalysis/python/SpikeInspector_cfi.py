c8823d7385a860c3182362fe00fcacaa95438465
troxlo <>
2010-11-03 18:17:57 +0000

    spikes

---
+++ PhotonAnalysis/python/SpikeInspector_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+spikeInspector = cms.EDAnalyzer('SpikeInspector',
+                               ebSuperClusterCollection  = cms.untracked.InputTag("correctedIslandBarrelSuperClusters"),
+                               ebReducedRecHitCollection = cms.untracked.InputTag("ecalRecHit","EcalRecHitsEB"),
+                               eeReducedRecHitCollection = cms.untracked.InputTag("ecalRecHit","EcalRecHitsEE"),
+                               swissCut                  = cms.untracked.double(0.95)
+)
