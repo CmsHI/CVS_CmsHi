3b5b2d0a961f418fd43e4952a182a8eb4716c0d6
yilmaz <>
2010-11-04 10:25:09 +0000

    common tools

--- Analysis2010/python/CommonFunctions_cff.py
+++ Analysis2010/python/CommonFunctions_cff.py
@@ -24,22 +24,22 @@ def disableMC(process):
 def overrideCentrality(process):
     process.GlobalTag.toGet = cms.VPSet(
         cms.PSet(record = cms.string("HeavyIonRcd"),
-                 tag = cms.string("CentralityTable_PixelHits40_Hydjet2760GeV_v0_mc"),
+                 tag = cms.string("CentralityTable_PixelHits40_Hydjet2760GeV_v1_mc"),
                  connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
                  label = cms.untracked.string("PixelHitsHydjet_2760GeV")
                  ),
         cms.PSet(record = cms.string("HeavyIonRcd"),
-                 tag = cms.string("CentralityTable_PixelHits40_AMPT2760GeV_v0_mc"),
+                 tag = cms.string("CentralityTable_PixelHits40_AMPT2760GeV_v1_mc"),
                  connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
                  label = cms.untracked.string("PixelHitsAMPT_2760GeV")
                  ),
         cms.PSet(record = cms.string("HeavyIonRcd"),
-                 tag = cms.string("CentralityTable_HFhits40_Hydjet2760GeV_v0_mc"),
+                 tag = cms.string("CentralityTable_HFhits40_Hydjet2760GeV_v1_mc"),
                  connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
                  label = cms.untracked.string("HFhitsHydjet_2760GeV")
                                   ),
         cms.PSet(record = cms.string("HeavyIonRcd"),
-                 tag = cms.string("CentralityTable_HFhits40_AMPT2760GeV_v0_mc"),
+                 tag = cms.string("CentralityTable_HFhits40_AMPT2760GeV_v1_mc"),
                  connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
                  label = cms.untracked.string("HFhitsAMPT_2760GeV")
                  )
