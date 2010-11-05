0adfb9cb9fc9e7bb813ed014815bfe7cd3c4408a
pshukla <>
2010-11-05 12:09:07 +0000

    One macro

--- Z0Ana/ZToMuMu/test/FitFunctions.h
+++ Z0Ana/ZToMuMu/test/FitFunctions.h
@@ -1,4 +1,7 @@
+/////////////////////////////
 // Different Fit functions
+// Heavy Ion Dilepton PiNG
+// Prashant Shukla and Vineet Kumar
 // Use as follows:
 // TF1 *fitFunction = new TF1("fitFunction", RBWPol2, 0, 200, 6);
 // histMass->Fit("fitFunction","Q0","MER", 20, 120);
@@ -9,7 +12,16 @@
 Double_t RBW(Double_t *x, Double_t *par)
 {
   Double_t rbw = par[0]*x[0]*par[1]*par[2]*0.5/TMath::Pi()/TMath::Max(1.e-10,(x[0]*x[0]-par[1]*par[1]) * (x[0]*x[0]-par[1]*par[1])+par[1]*par[1]*par[2]*par[2]);
-  return rbw;
+ 
+  //  Double_t rbw = par[0]*x[0]*par[1]*par[2]/TMath::Max(1.e-10,(x[0]*x[0]-par[1]*par[1]) * (x[0]*x[0]-par[1]*par[1])+par[1]*par[1]*par[2]*par[2]);
+
+ return rbw;
+
+}
+
+Double_t RBWPol1(Double_t *x, Double_t *par)
+{
+  return RBW(x,par) + Pol1(x,&par[3]);
 }
 
 Double_t RBWPol2(Double_t *x, Double_t *par)
@@ -40,8 +52,13 @@ Double_t RBWExp(Double_t *x, Double_t *par)
 
 Double_t BW(Double_t *x, Double_t *par)
 {
-  Double_t rbw = par[0]*par[2]/TMath::Max(1.e-10,(x[0]*x[0]-par[1]*par[1]) * (x[0]*x[0]-par[1]*par[1]) + 0.25*par[2]*par[2]);
+  Double_t rbw = par[0]*par[2]*0.5/TMath::Pi()/TMath::Max(1.e-10,(x[0]*x[0]-par[1]*par[1]) * (x[0]*x[0]-par[1]*par[1]) + 0.25*par[2]*par[2]);
   return rbw;
+}
+
+Double_t BWPol1(Double_t *x, Double_t *par)
+{
+  return BW(x,par) + Pol1(x,&par[3]);
 }
 
 Double_t BWPol2(Double_t *x, Double_t *par)
@@ -72,16 +89,27 @@ Double_t BWExp(Double_t *x, Double_t *par)
 
 Double_t Gaus(Double_t *x, Double_t *par)
 {
+  Double_t invsq2pi = 0.3989422804014;   // 1/sqrt(2 pi)
   Double_t arg = 0;
   if (par[2]) arg = (x[0] - par[1])/par[2];
   Double_t gau = par[0]*TMath::Exp(-0.5*arg*arg);
-  return gau;
+  return gau*invsq2pi/par[2];
 }
+
+
+
+Double_t GausPol1(Double_t *x, Double_t *par) 
+{
+  return Gaus(x,par) + Pol1(x,&par[3]);
+}
+
 
 Double_t GausPol2(Double_t *x, Double_t *par) 
 {
   return Gaus(x,par) + Pol2(x,&par[3]);
 }
+
+
 
 Double_t GausPol3(Double_t *x, Double_t *par) 
 {
@@ -134,4 +162,65 @@ Double_t Exp(Double_t *x, Double_t *par)
 }
 
 
+Double_t RBWGaus(Double_t *x, Double_t *par) {
+  ////// RBW convoluted with gaussian ////// 
+  //Fit parameters:
+  //par[0]=Normalization
+  //par[1]=Mean of RBW
+  //par[2]=Width of RBW
+  //par[3]=Width (sigma) of convoluted Gaussian function
+  
+  // Range of convolution integral
+  Double_t np = 100.0;      // number of convolution steps
+  Double_t xlow = x[0] - 5.0 * par[3];
+  Double_t xupp = x[0] + 5.0 * par[3];
+  Double_t step = (xupp-xlow) / np;
 
+  // Convolution integral of Landau and Gaussian by sum
+  // Variables
+  Double_t xx, fland, sum = 0.0;
+
+  for(Double_t i=1.0; i<=np/2; i++) {
+    xx = xlow + (i-.5) * step;
+    fland = RBWC(xx, par[1], par[2]);
+    sum += fland * GausC(x[0], xx, par[3]);
+    
+    xx = xupp - (i-.5) * step;
+    fland = RBWC(xx, par[1], par[2]);
+    sum += fland * GausC(x[0], xx, par[3]);
+  }
+  
+  return (par[0] * step * sum);
+}
+
+Double_t RBWGausPol1(Double_t *x, Double_t *par)
+{
+  return RBWGaus(x,par) + Pol1(x,&par[4]);
+}
+
+
+Double_t RBWGausPol2(Double_t *x, Double_t *par)
+{
+  return RBWGaus(x,par) + Pol2(x,&par[4]);
+}
+
+
+Double_t RBWGausPol3(Double_t *x, Double_t *par)
+{
+  return RBWGaus(x,par) + Pol3(x,&par[4]);
+}
+
+
+Double_t RBWC(Double_t x, Double_t par1, Double_t par2)
+{
+  Double_t rbw = x*par1*par2*0.5/TMath::Pi()/TMath::Max(1.e-10,(x*x-par1*par1) * (x*x-par1*par1) + par1*par1*par2*par2);
+ return rbw;
+}
+
+Double_t GausC(Double_t x, Double_t par1, Double_t par2)
+{
+  Double_t invsq2pi = 0.3989422804014;   // 1/sqrt(2 pi)
+  Double_t arg = 0;
+  if (par2) arg = (x - par1)/par2;
+  return TMath::Exp(-0.5*arg*arg)*invsq2pi/par2;
+}
