7c15af3c86769d001d4cd6021dbbdf6368c40934
frankma <>
2010-11-08 13:54:19 +0000

    update to the latest fixes

--- Analysis2010/scripts/setup.sh
+++ Analysis2010/scripts/setup.sh
@@ -4,9 +4,9 @@ indir=`pwd`
 cd $CMSSW_BASE/src/
 
 
-cvs co -r V00-00-06 CondFormats/HIObjects
-cvs co -r V00-01-20 DataFormats/HeavyIonEvent
-cvs co -r V00-01-18 RecoHI/HiCentralityAlgos
+cvs co -r CondFormats/HIObjects
+cvs co -r DataFormats/HeavyIonEvent
+cvs co -r RecoHI/HiCentralityAlgos
 cvs co -d CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
 
 # Optional for other pings than high-pt:
