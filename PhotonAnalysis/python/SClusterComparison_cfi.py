1038f755dc37e07e38c7a714626003f3f0362b58
troxlo <>
2010-11-12 00:36:30 +0000

    *** empty log message ***

---
+++ PhotonAnalysis/python/SClusterComparison_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+SClusterComparison = cms.EDAnalyzer("SClusterComparison",
+        cleanedHybridSuperClusters = cms.InputTag("cleanedHybridSuperClusters"),
+        correctedHybridSuperClusters = cms.InputTag("correctedHybridSuperClusters"),
+        hybridSuperClusters = cms.InputTag("hybridSuperClusters"),
+        uncleanedHybridSuperClusters = cms.InputTag("uncleanedHybridSuperClusters"),
+        islandBarrelSuperClusters = cms.InputTag("islandSuperClusters","islandBarrelSuperClusters"),
+        correctedIslandBarrelSuperClusters = cms.InputTag("correctedIslandBarrelSuperClusters")
+        )
+
