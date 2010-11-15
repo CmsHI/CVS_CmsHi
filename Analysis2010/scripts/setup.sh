624381cb4b2d9fc2e37784ba6c37c324fae3a8e3
frankma <>
2010-11-15 14:06:35 +0000

    fix typo

--- Analysis2010/scripts/setup.sh
+++ Analysis2010/scripts/setup.sh
@@ -4,10 +4,10 @@ indir=`pwd`
 cd $CMSSW_BASE/src/
 
 
-cvs co -r CondFormats/HIObjects
-cvs co -r DataFormats/HeavyIonEvent
-cvs co -r RecoHI/HiCentralityAlgos
-cvs co -d CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
+cvs co CondFormats/HIObjects
+cvs co DataFormats/HeavyIonEvent
+cvs co RecoHI/HiCentralityAlgos
+#cvs co -d CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
 
 # Optional for other pings than high-pt:
 cvs co -d CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
