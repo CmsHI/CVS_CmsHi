e33d10d0354eca5728ead94f9b849e2829056b47
silvest <>
2010-11-25 00:42:39 +0000

    with centrality updated

--- Z0Ana/DimuonAnalyzer/test/dimuonanalyzer_cfg.py
+++ Z0Ana/DimuonAnalyzer/test/dimuonanalyzer_cfg.py
@@ -12,17 +12,21 @@ process = cms.Process("Demo")
 #                                    )
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = 'START39_V4::All' #for MC# Make sure you have the right global tag (*)
-'''
+#process.GlobalTag.globaltag = 'GR10_P_V12::All'
+
 from CmsHi.Analysis2010.CommonFunctions_cff import *
 overrideCentrality(process)
-'''
+
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-from QuickPlots.DimuonAnalyzer.HIExpressHIrun2010_v1_cfi import *
+#from QuickPlots.DimuonAnalyzer.HIExpressHIrun2010_v1_cfi import *
 process.source = cms.Source("PoolSource",
     noEventSort = cms.untracked.bool(True),
     duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
    #                         fileNames = readFiles,
-    fileNames = cms.untracked.vstring('rfio:///castor/cern.ch/user/d/dmoon/cms390pre5/Hydjet_MinBias_2.76TeV_Z0_Flat_Emb_HLT_Reco/Hydjet_MinBias_2.76TeV_Z0_Flat_Emb_HLT_Reco_1.root' )
+    fileNames = cms.untracked.vstring(
+"file:/tmp/jrobles/hiRecoDM_RECO.root")
+#'rfio:///castor/cern.ch/user/d/dmoon/cms390pre5/Hydjet_MinBias_2.76TeV_Z0_Flat_Emb_HLT_Reco/Hydjet_MinBias_2.76TeV_Z0_Flat_Emb_HLT_Reco_1.root' )
                            )
 
 
@@ -31,18 +35,10 @@ process.HeavyIonGlobalParameters = cms.PSet(
     nonDefaultGlauberModel = cms.string("Hydjet_2760GeV"),
     centralitySrc = cms.InputTag("hiCentrality")
     )
-'''
-process.GlobalTag.toGet = cms.VPSet(
-    cms.PSet(record = cms.string("HeavyIonRcd"),
-             tag = cms.string("CentralityTable_HFhits40_Hydjet2760GeV_v1_mc"),
-             connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS")
-             )
-    )
-'''
 
 
 process.demo = cms.EDAnalyzer('DimuonAnalyzer',
-                              genParticle  = cms.InputTag("hiGenParticles"),#no mixing:genParticles
+                              genParticle  = cms.InputTag("genParticles"),
                               muonTracks   = cms.untracked.InputTag("globalMuons"),
 #                              trackTracks  = cms.untracked.InputTag("hiGlobalPrimTracks"), # pp reco: "generalTracks"),   
                               trackTracks  = cms.untracked.InputTag("hiSelectedTracks"), # pp reco: "generalTracks"),   
@@ -53,14 +49,14 @@ process.demo = cms.EDAnalyzer('DimuonAnalyzer',
                               ptMinDimuon  = cms.double(0.),
                               etaMaxMuon   = cms.double(2.5),
                               etaMinMuon   = cms.double(-2.5),
-                              ptMinMuon    = cms.double(3.5),
+                              ptMinMuon    = cms.double(1.),
                               etaMaxTrack  = cms.double(2.5),
                               etaMinTrack  = cms.double(-2.5),
                               ptMinTrack   = cms.double(1.),
                               doRecoSingleMuon = cms.bool(True),  #To check reconstructed muons = True
                               doReco       = cms.bool(True),  #To check various reconstructed objects = True
-                              doMC         = cms.bool(False), #To read various generation level informations = True
-                              doSim        = cms.bool(False), #To do SimTrack checking = True
+                              doMC         = cms.bool(True), #To read various generation level informations = True
+                              doSim        = cms.bool(True), #To do SimTrack checking = True
                               doSignal     = cms.bool(False), #To see signal from generation = True
                               doZ0check    = cms.bool(False), #To see mother of gen muons and daugher of gen Z0 = True
                               genSignal    = cms.untracked.InputTag("hiSignal"),
