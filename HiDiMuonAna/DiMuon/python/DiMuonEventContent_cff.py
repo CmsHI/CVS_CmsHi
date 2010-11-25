192a6ca8a4a53392b2077457ef556488291fadc9
pshukla <>
2010-11-25 15:21:53 +0000

    New EvCon for mu quality cuts

--- HiDiMuonAna/DiMuon/python/DiMuonEventContent_cff.py
+++ HiDiMuonAna/DiMuon/python/DiMuonEventContent_cff.py
@@ -1,13 +1,39 @@
 import FWCore.ParameterSet.Config as cms
 
-DiMuonContent = cms.PSet(
-    outputCommands = cms.untracked.vstring(
+##### event content for heavy-ion analysis objects
+from Configuration.EventContent.EventContentHeavyIons_cff import *
+from RecoHI.HiMuonAlgos.RecoHiMuon_EventContent_cff import *
+
+
+hiCommon = cms.PSet(
+    outputCommands = cms.untracked.vstring('drop *',
+                                           'keep *_TriggerResults_*_HLT',
+                                           'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*',
+                                           'keep recoVertexs_hiSelectedVertex_*_*',
+                                           'keep *_heavyIon_*_*',
+                                           'keep *_hiCentrality_*_*',
+                                           'keep *_hiEvtPlane_*_*'
+                                           )
+    )
+
+
+
+
+DiMuonContent = hiCommon.clone()
+DiMuonContent.outputCommands.extend(cms.untracked.vstring(
     #'keep *',    
+    
+    'keep *_offlineBeamSpot_*_*',
+    'keep *_hiSelectedTracks_*_*',
+    'keep *_towerMaker_*_*',
+    'keep *_hiGlobalPrimTracks_*_*',
+    'keep *_hiPixel3PrimTracks_*_*',
+
+
     'keep *_generator_*_*',
     'keep *_genParticles_*_*',
     'keep *_hiGenParticles_*_*',
-    'keep *_hiCentrality_*_*',
-    'keep *_hiEvtPlane_*_*',
+    'keep *_hiSignal_*_*',
     'keep patMuons_*_*_*',
     'keep recoMuons_muons_*_*',
     'keep *_dimuonsGlobal_*_*',
@@ -23,10 +49,11 @@ DiMuonContent = cms.PSet(
     'keep *_GenDimuons_*_*',
     'keep *_dimuons_*_*',
     'keep *_dimuonsSameCharge_*_*',
-    'keep *_ditracksSiliconTrk_*_*'
-    )
+    'keep *_ditracksSiliconTrk_*_*')
     )
 
+DiMuonContent.outputCommands.extend(RecoMuonAOD.outputCommands)
+DiMuonContent.outputCommands.extend(RecoHiMuonAOD.outputCommands)
 
 
 
