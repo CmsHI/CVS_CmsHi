6e5ad925290d7fbf49dae158d9c872ee34fd4956
kimy <>
2011-02-06 15:48:53 +0000

    *** empty log message ***

--- PhotonAnalysis/python/isoConeInspector_cfi.py
+++ PhotonAnalysis/python/isoConeInspector_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-isoConeMap = cms.EDAnalyzer("IsoConeInspector",
-                            photonProducer = cms.InputTag("photons"),
-                            ebReducedRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
-                            eeReducedRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
-                            doSpikeClean              = cms.untracked.bool(False),
-                            etCut                     = cms.untracked.double(15),
-                            etaCut                    = cms.untracked.double(1.479)
-                            )
+isoConeInspector = cms.EDAnalyzer("IsoConeInspector",
+                                  photonProducer = cms.InputTag("photons"),
+                                  ebReducedRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
+                                  eeReducedRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+                                  doSpikeClean              = cms.untracked.bool(False),
+                                  etCut                     = cms.untracked.double(15),
+                                  etaCut                    = cms.untracked.double(1.479)
+                                  )
 
