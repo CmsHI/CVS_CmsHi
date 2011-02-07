24a79262e8e7fa1c33f21eba5c07c0b141f5f254
kimy <>
2011-02-07 17:30:23 +0000

    added centrality parameter

--- PhotonAnalysis/python/isoConeInspector_cfi.py
+++ PhotonAnalysis/python/isoConeInspector_cfi.py
@@ -5,6 +5,7 @@ isoConeInspector = cms.EDAnalyzer("IsoConeInspector",
                                   ebReducedRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
                                   eeReducedRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
                                   doSpikeClean              = cms.untracked.bool(False),
+                                  doStoreCentrality         = cms.untracked.bool(True),
                                   etCut                     = cms.untracked.double(15),
                                   etaCut                    = cms.untracked.double(1.479)
                                   )
