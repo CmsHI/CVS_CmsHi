c4521b9114099c1b5af20e9a42a0c213927d22a0
frankma <>
2011-08-03 16:18:47 +0000

    add multiphoton analyzer

--- JetAnalysis/scripts/setup39X.sh
+++ JetAnalysis/scripts/setup39X.sh
@@ -1,7 +1,14 @@
+#Centrality
+cvs co          CondFormats/HIObjects
+cvs co          DataFormats/HeavyIonEvent
+cvs co          RecoHI/HiCentralityAlgos
+cvs co -d       CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
+
 ## hiGoodMergedTracks and the track analyzer
 cvs co -d       edwenger/HiVertexAnalyzer UserCode/edwenger/HiVertexAnalyzer
 cvs co -d       edwenger/HiTrkEffAnalyzer UserCode/edwenger/HiTrkEffAnalyzer
-cvs co -d       edwenger/HiTrkEffAnalyzer/python/ UserCode/edwenger/HiTrkEffAnalyzer/python/
+cvs co -d       Appeltel/PixelTracksRun2010 UserCode/Appeltel/PixelTracksRun2010
+cvs co -d       MitHig/PixelTrackletAnalyzer UserCode/MitHig/PixelTrackletAnalyzer
 
 # pat macros
 cvs co -d      MNguyen/patMacrosForMC  UserCode/MNguyen/patMacrosForMC
@@ -11,22 +18,25 @@ cvs co         HeavyIonsAnalysis/Configuration
 
 # Jet and HI Software
 cvs co -d       MNguyen/InclusiveJetAnalyzer UserCode/MNguyen/InclusiveJetAnalyzer
-
 cvs co -r       V05-01-09 RecoJets/JetProducers
 cvs co          RecoHI/HiJetAlgos
-addpkg           RecoHI/Configuration
-
-#Centrality
-cvs co          CondFormats/HIObjects
-cvs co          DataFormats/HeavyIonEvent
-cvs co          RecoHI/HiCentralityAlgos
-cvs co -r cmssw39x_branch -d       CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
+addpkg          RecoHI/Configuration
 
 # needed to get PFTowers to work
-cvs co -d CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
+cvs co -r cmssw39x_branch -d CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
 cp CmsHi/JetAnalysis/modifiedFiles/VirtualJetProducer.cc RecoJets/JetProducers/plugins/
 cp CmsHi/JetAnalysis/modifiedFiles/PileUpSubtractor.cc RecoJets/JetProducers/src/
 cp CmsHi/JetAnalysis/modifiedFiles/PileUpSubtractor.h RecoJets/JetProducers/interface/
+
+# gamma analysis
+cvs co RecoHI/HiEgammaAlgos                  # for 3_9_8 reconstruction   including spike cleaner 
+cvs co -d CmsHi/PhotonAnalysis UserCode/CmsHi/PhotonAnalysis
+cvs co -d UserCode/HafHistogram UserCode/CmsHi/HafHistogram
+# electron reco sequence
+rm -rf RecoHI/HiEgammaAlgos/python
+cvs co -r CMSSW_3_9_9_patch1 RecoHI/HiEgammaAlgos/python
+cvs co RecoHI/HiEgammaAlgos/python/HiCoreTools.py
+cvs co RecoHI/HiEgammaAlgos/python/HiHelperTools.py
 
 #ecal and hcal cleaning
 #hcal
@@ -36,11 +46,9 @@ cvs co -r V00-07-21 RecoLocalCalo/HcalRecAlgos/interface/HBHETimingShapedFlag.h
 cvs co -r V00-00-18 JetMETAnalysis/HcalReflagging
 cvs co -d HcalFilter UserCode/MTonjes/HcalFilter
 
-#ecal requires some code from the photon analyzer, 'rm' commands are to truncate the endless chain of packages obviously, this should be cleaned up
-cvs co -d CmsHi/PhotonAnalysis UserCode/CmsHi/PhotonAnalysis
-rm CmsHi/PhotonAnalysis/plugins/MultiPhotonAnalyzer.*
-rm CmsHi/PhotonAnalysis/plugins/SinglePhotonAnalyzer.*
-cvs co -d UserCode/HafHistogram UserCode/CmsHi/HafHistogram
+# keep 39X version for spike cleaner.
+rm RecoHI/HiEgammaAlgos/plugins/HiSpikeCleaner.cc
+cvs co -r HI_HLT_2010_11_20_fix2 RecoHI/HiEgammaAlgos/plugins/HiSpikeCleaner.cc
 
 # openhlt
 cvs co -r V03-03-08 HLTrigger/HLTanalyzers
