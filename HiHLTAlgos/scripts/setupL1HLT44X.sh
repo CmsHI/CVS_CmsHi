a289bf6d5841e0f81dba0684bcb49d18b3b03849
frankma <>
2011-10-10 13:54:21 +0000

    update with new master menu and tagged hltbitana

--- HiHLTAlgos/scripts/setupL1HLT44X.sh
+++ HiHLTAlgos/scripts/setupL1HLT44X.sh
@@ -5,12 +5,11 @@ cvs co RecoLuminosity/LumiDB
 addpkg L1Trigger/Configuration
 
 # HLT
-cvs co -r V12-00-03 HLTrigger/Configuration
-addpkg HLTrigger/HLTanalyzers
+cvs co -r V12-02-45 HLTrigger/Configuration
+cvs co -r V03-08-14 HLTrigger/HLTanalyzers
 
 # get menu
-hltGetConfiguration --cff --offline --data /users/frankma/devCMSSW_4_2_0/HIonV226 --type HIon --unprescale > HLT_HIon_data_cff.py
-mv HLT_HIon_data_cff.py HLTrigger/Configuration/python/
+hltGetConfiguration --cff --offline --data  hltGetConfiguration --cff --offline --data /users/frankma/devCMSSW_4_2_0/HIonV976 --type HIon --unprescale > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_data_cff.py
 
 # hiGoodMergedTracks and the track analyzer
 cvs co -d       edwenger/HiVertexAnalyzer UserCode/edwenger/HiVertexAnalyzer
@@ -18,13 +17,13 @@ cvs co -d       edwenger/HiTrkEffAnalyzer UserCode/edwenger/HiTrkEffAnalyzer
 
 # jet analyzers
 cvs co          RecoHI/HiJetAlgos
+cvs co         HeavyIonsAnalysis/Configuration
 cvs co -d       CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
-cvs co -d       MNguyen/InclusiveJetAnalyzer UserCode/MNguyen/InclusiveJetAnalyzer
+rm CmsHi/JetAnalysis/python/EGammaAnalyzers_cff.py # tmp fix
+rm CmsHi/JetAnalysis/python/ExtraEGammaReco_cff.py # tmp fix
+rm CmsHi/JetAnalysis/python/EventSelection_cff.py # tmp fix
 
 # trigger analyzers
 cvs co -d       CmsHi/HiHLTAlgos UserCode/CmsHi/HiHLTAlgos
-cp CmsHi/HiHLTAlgos/modifiedFiles/cmssw44X/EventHeader.cc HLTrigger/HLTanalyzers/src
-cp CmsHi/HiHLTAlgos/modifiedFiles/cmssw44X/HLTBitAnalyzer.cc HLTrigger/HLTanalyzers/src
-cp CmsHi/HiHLTAlgos/modifiedFiles/cmssw44X/HLTBitAnalyzer.h HLTrigger/HLTanalyzers/interface
 
 scram build -c
