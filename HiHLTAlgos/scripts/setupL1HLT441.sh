135b3a650cb729fc10ac0742e78d8e6110de757e
frankma <>
2011-10-25 17:17:21 +0000

    added analyzers

--- HiHLTAlgos/scripts/setupL1HLT441.sh
+++ HiHLTAlgos/scripts/setupL1HLT441.sh
@@ -8,11 +8,17 @@ cvs co -r V03-00-07     L1TriggerConfig/L1GtConfigProducers
 hltGetConfiguration --cff --offline --data /dev/CMSSW_4_2_0/HIon/V337 --type HIon --unprescale > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_data_cff.py
 hltGetConfiguration --cff --offline --data /users/frankma/devCMSSW_4_2_0/HIonV1047 --type HIon --unprescale > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_Jet_data_cff.py
 
-# trigger analyzers
-cvs co -d       CmsHi/HiHLTAlgos UserCode/CmsHi/HiHLTAlgos
-
 # custom l1 menu from xml
 addpkg L1Trigger/Configuration
 cp -v CmsHi/HiHLTAlgos/modifiedFiles/L1Trigger_custom.py L1Trigger/Configuration/python/
+
+# analyzers
+cvs co UserCode/L1TriggerDPG
+cvs co -d       CmsHi/HiHLTAlgos UserCode/CmsHi/HiHLTAlgos
+cvs co -d       edwenger/HiVertexAnalyzer UserCode/edwenger/HiVertexAnalyzer
+cvs co -d       edwenger/HiTrkEffAnalyzer UserCode/edwenger/HiTrkEffAnalyzer
+cvs co          RecoHI/HiJetAlgos
+cvs co         HeavyIonsAnalysis/Configuration
+cvs co -r hi441_1 -d CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
 
 scram build -c
