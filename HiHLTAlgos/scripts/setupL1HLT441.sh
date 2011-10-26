f4a55fa49da0c6dd3c2ca1b7fd470ab9d0f07a33
malek <>
2011-10-26 08:55:02 +0000

    update l1 menu to 1.3

--- HiHLTAlgos/scripts/setupL1HLT441.sh
+++ HiHLTAlgos/scripts/setupL1HLT441.sh
@@ -2,11 +2,13 @@
 # l1/HLT menu tags
 #cvs co -r V12-03-18     HLTrigger/Configuration
 cvs co -r V12-03-21     HLTrigger/Configuration
-cvs co -r V03-00-07     L1TriggerConfig/L1GtConfigProducers
+cvs co -r V03-00-08     L1TriggerConfig/L1GtConfigProducers
 
 # get hlt menu
-hltGetConfiguration --cff --offline --data /dev/CMSSW_4_2_0/HIon/V337 --type HIon --unprescale > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_data_cff.py
+hltGetConfiguration --cff --offline --data /dev/CMSSW_4_2_0/HIon --type HIon --unprescale > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_data_cff.py
 hltGetConfiguration --cff --offline --data /users/frankma/devCMSSW_4_2_0/HIonV1047 --type HIon --unprescale > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_Jet_data_cff.py
+
+cvs co -d       CmsHi/HiHLTAlgos UserCode/CmsHi/HiHLTAlgos
 
 # custom l1 menu from xml
 addpkg L1Trigger/Configuration
@@ -14,7 +16,6 @@ cp -v CmsHi/HiHLTAlgos/modifiedFiles/L1Trigger_custom.py L1Trigger/Configuration
 
 # analyzers
 cvs co UserCode/L1TriggerDPG
-cvs co -d       CmsHi/HiHLTAlgos UserCode/CmsHi/HiHLTAlgos
 cvs co -d       edwenger/HiVertexAnalyzer UserCode/edwenger/HiVertexAnalyzer
 cvs co -d       edwenger/HiTrkEffAnalyzer UserCode/edwenger/HiTrkEffAnalyzer
 cvs co          RecoHI/HiJetAlgos
