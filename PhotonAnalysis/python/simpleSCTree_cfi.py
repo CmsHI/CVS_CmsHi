45e78280e0856577b589cf3afc8c9d895ba95b16
kimy <>
2011-11-02 21:49:08 +0000

    *** empty log message ***

--- PhotonAnalysis/python/simpleSCTree_cfi.py
+++ PhotonAnalysis/python/simpleSCTree_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-scAnaUC = cms.EDAnalyzer("ClusterTreeMaker",
-                         scTagB = cms.untracked.string("correctedIslandBarrelSuperClusters"), #cleanedBarrelSC"),
-                         scTagE = cms.untracked.string("correctedIslandEndcapSuperClusters"),
-                         ebRecHitCollection = cms.untracked.InputTag( 'hltEcalRecHitAll','EcalRecHitsEB' ),
-                         eeRecHitCollection = cms.untracked.InputTag( 'hltEcalRecHitAll','EcalRecHitsEE' ),
-                         etCut              = cms.untracked.double(8)
-                         )
+simpleSCTree = cms.EDAnalyzer("ClusterTreeMaker",
+                              scTagB = cms.untracked.string("correctedIslandBarrelSuperClusters"), #cleanedBarrelSC"),
+                              scTagE = cms.untracked.string("correctedIslandEndcapSuperClusters"),
+                              ebRecHitCollection = cms.untracked.InputTag( 'hltEcalRecHitAll','EcalRecHitsEB' ),
+                              eeRecHitCollection = cms.untracked.InputTag( 'hltEcalRecHitAll','EcalRecHitsEE' ),
+                              etCut              = cms.untracked.double(8)
+                              )
