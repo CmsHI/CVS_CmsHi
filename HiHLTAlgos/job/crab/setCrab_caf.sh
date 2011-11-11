b215eeb07082d5532b832b016be9704a7c0f6b7d
frankma <>
2011-11-11 18:21:46 +0000

    book-keep cmssw area

--- HiHLTAlgos/job/crab/setCrab_caf.sh
+++ HiHLTAlgos/job/crab/setCrab_caf.sh
@@ -5,7 +5,7 @@ voms-proxy-init --valid 168:00 -voms cms
 #-pwstdin < $HOME/.grid-cert-passphrase
 voms-proxy-info --all
 source /afs/cern.ch/cms/sw/cmsset_default.sh
-cd $CMSSW_BASE/src
+cd /afs/cern.ch/cms/CAF/CMSPHYS/PHYS_HEAVYIONS/openHLT/CMSSW_4_4_2_patch4/src
 eval `scram runtime -sh`
 source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh
 cd -
