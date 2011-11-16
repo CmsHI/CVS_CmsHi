e1008c585877ad1149b610924c41386b147d26bb
yjlee <>
2011-11-16 14:08:40 +0000

    Updated MET tree and etc.

--- JetAnalysis/macros/forest/generateSetupTree/makeSetupCode.C
+++ JetAnalysis/macros/forest/generateSetupTree/makeSetupCode.C
@@ -1,17 +1,19 @@
 #include "makeClass.h"
 
-void makeSetupCode(char *infname = "/d100/yjlee/hiForest/merged_HI2010_SD_Photon40_prod02.root")
+void makeSetupCode(char *infname = "../merged_Run181531_HIExpressPhysics_Part.root")
 {
 
   TFile *inf = new TFile(infname);  
 
   TTree *hltTree      = (TTree*) inf->Get("hltanalysis/HltTree");
   TTree *skimTree     = (TTree*) inf->Get("skimanalysis/HltTree");
-  TTree *photonTree   = (TTree*) inf->Get("NTuples/Analysis");
+  TTree *photonTree   = (TTree*) inf->Get("multiPhotonAnalyzer/photon");
+  TTree *evtTree      = (TTree*) inf->Get("hiEvtAnalyzer/HiTree");
   TTree *trackTree    = (TTree*) inf->Get("anaTrack/trackTree");
   TTree *jetTree      = (TTree*) inf->Get("akPu3PFJetAnalyzer/t");
   TTree *hitTree      = (TTree*) inf->Get("rechitanalyzer/hbhe");
   TTree *metTree      = (TTree*) inf->Get("anaMET/metTree");
+  TTree *pfTree      = (TTree*) inf->Get("pfcandAnalyzer/pfTree");
 
   Int_t phfCoincFilter;
   Int_t ppurityFractionFilter;
@@ -32,8 +34,10 @@ void makeSetupCode(char *infname = "/d100/yjlee/hiForest/merged_HI2010_SD_Photon
   if (hltTree) makeClass(hltTree,"Hlt","");
   if (skimTree) makeClass(skimTree,"Skim","");
   if (photonTree) makeClass(photonTree,"Photon","");
+  if (evtTree) makeClass(evtTree,"Evt","");
   if (trackTree) makeClass(trackTree,"Track","");
   if (jetTree) makeClass(jetTree,"Jet","");
   if (hitTree) makeClass(hitTree,"Hit","");
   if (metTree) makeClass(metTree,"Met","");
+  if (pfTree) makeClass(pfTree,"PF","");
 }
