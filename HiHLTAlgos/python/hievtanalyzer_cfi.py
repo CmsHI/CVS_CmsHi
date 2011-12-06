1fed1e7196e7bbd91509d35f40e1d222dab7adb1
yilmaz <>
2011-12-06 10:40:34 +0000

    evt plane

--- HiHLTAlgos/python/hievtanalyzer_cfi.py
+++ HiHLTAlgos/python/hievtanalyzer_cfi.py
@@ -4,9 +4,11 @@ hiEvtAnalyzer = cms.EDAnalyzer('HiEvtAnalyzer',
    Centrality    = cms.InputTag("hiCentrality"),
    CentralityBin = cms.InputTag("centralityBin"),
    EvtPlane      = cms.InputTag("hiEvtPlane","recoLevel"),
+   EvtPlane      = cms.InputTag("hiEvtPlaneFlat",""),                               
    Vertex        = cms.InputTag("hiSelectedVertex"),
    HiMC          = cms.InputTag("heavyIon"),
    doEvtPlane    = cms.bool(False),
+   doEvtPlaneFlat    = cms.bool(False),                               
    doVertex      = cms.bool(True),
    doMC          = cms.bool(False)
 )
