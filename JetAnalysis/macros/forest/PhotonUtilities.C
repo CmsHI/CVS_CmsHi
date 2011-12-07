85ecc702c49f75c4874a45d08d9b481e920a95c7
kimy <>
2011-12-07 06:52:52 +0000

    changed the sum iso cut in goodPhoton

--- JetAnalysis/macros/forest/PhotonUtilities.C
+++ JetAnalysis/macros/forest/PhotonUtilities.C
@@ -35,7 +35,7 @@ bool HiForest::isGoodPhoton(int j)
       if (photon.isEle[j]) return 0;
       if ((photon.rawEnergy[j]/photon.energy[j])<0.5) return 0;
       if (photon.sigmaIetaIeta[j]>0.010) return 0;
-      if ((photon.cr4[j]+photon.cc4[j]+photon.ct4PtCut20[j])>5) return 0;
+      if ((photon.cr4[j]+photon.cc4[j]+photon.ct4PtCut20[j])/0.9> 5.0 ) return 0;
    } else {
       // Endcap photon
       return 0;  // Need to update to include endcap photons
