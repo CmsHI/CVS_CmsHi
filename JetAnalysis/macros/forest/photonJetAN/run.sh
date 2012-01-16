0fbd55d3327f2911b79312a59be3fb80f6da6947
frankma <>
2012-01-16 23:30:43 +0000

    pt scan

--- JetAnalysis/macros/forest/photonJetAN/run.sh
+++ JetAnalysis/macros/forest/photonJetAN/run.sh
@@ -1,12 +1,17 @@
-for m in plotPtRatioSignal_AllCent4.C plotDeltaPhiSignal_AllCent4.C; do
+#for m in plotPtRatioSignal_AllCent4.C plotDeltaPhiSignal_AllCent4.C; do
+for m in plotPtRatioSignal_AllCent4.C; do
   for log in 0 1; do
     for doChk in 0 1; do
-      root -b -q $m+'(0,1,1,1,60,30,'$log','$doChk')'
-      root -b -q $m+'(2,1,1,1,60,30,'$log','$doChk')'
-      if [ $m == "plotPtRatioSignal_AllCent4.C" ]; then
-	root -b -q $m+'(0,2,1,1,60,30,'$log','$doChk')'
-	root -b -q $m+'(2,2,1,1,60,30,'$log','$doChk')'
-      fi
+      for phoMin in 60 65 70; do
+	for jetMin in 30 35; do
+	  root -b -q $m+'(0,1,1,1,'$phoMin','$jetMin','$log','$doChk')'
+	  root -b -q $m+'(2,1,1,1,'$phoMin','$jetMin','$log','$doChk')'
+	  if [ $m == "plotPtRatioSignal_AllCent4.C" ]; then
+	    root -b -q $m+'(0,2,1,1,'$phoMin','$jetMin','$log','$doChk')'
+	    root -b -q $m+'(2,2,1,1,'$phoMin','$jetMin','$log','$doChk')'
+	  fi
+	done
+      done
     done
   done
 done
