5c5439d4ac5c768f82c756777273f21c62f65270
frankma <>
2012-02-03 19:11:31 +0000

    simpler

--- JetAnalysis/macros/forest/photonJetAN/run.sh
+++ JetAnalysis/macros/forest/photonJetAN/run.sh
@@ -5,27 +5,20 @@ mkdir -p $outdir
 
 doChk=0
 
-for m in plotPtRatioSignal_AllCent4_wSummary.C plotDeltaPhiSignal_AllCent4_wSummary.C; do
-  log=0
-  normMode=2
-  if [ $m=="plotDeltaPhiSignal_AllCent4_wSummary.C" ]; then
-    log=1
-    normMode=1
-  fi
-
-  # isolation schemes
-  for isolScheme in 0 2; do
-    
-    for ilog in 0 1; do
-      root -b -q $m+'('$isolScheme','$normMode',1,1,60,30,'$ilog','$doChk',"'$outdir'")'
-    done
-
-    # kinematic xchecks
-    for phoMin in 60 50 70; do
-      for jetMin in 30 35; do
-	root -b -q $m+'('$isolScheme','$normMode',1,1,'$phoMin','$jetMin','$log','$doChk',"'$outdir'")'
-      done
-    done
-
+# isolation schemes
+for isolScheme in 0 2; do
+  # standard
+  for ilog in 0 1; do
+    root -b -q plotPtRatioSignal_AllCent4_wSummary.C+'('$isolScheme',2,1,1,60,30,'$ilog','$doChk',"'$outdir'")'
+    #root -b -q plotDeltaPhiSignal_AllCent4_wSummary.C+'('$isolScheme',1,1,1,60,30,'$ilog','$doChk',"'$outdir'")'
   done
+
+  # kinematic xchecks
+  for phoMin in 60 50 70; do
+    for jetMin in 30 35; do
+      root -b -q plotPtRatioSignal_AllCent4_wSummary.C+'('$isolScheme',2,1,1,'$phoMin','$jetMin',0,'$doChk',"'$outdir'")'
+      #root -b -q plotDeltaPhiSignal_AllCent4_wSummary.C+'('$isolScheme',1,1,1,'$phoMin','$jetMin',1,'$doChk',"'$outdir'")'
+    done
+  done
+  # work done
 done
