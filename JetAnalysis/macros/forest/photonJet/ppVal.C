662ae3807963244a0ded92a06c8978a0f4b164e5
frankma <>
2012-02-09 05:41:36 +0000

    check ak5 in pp 7TeV

--- JetAnalysis/macros/forest/photonJet/ppVal.C
+++ JetAnalysis/macros/forest/photonJet/ppVal.C
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include "TFile.h"
 #include "TTree.h"
@@ -14,15 +15,15 @@
 using namespace std;
 
 void ppVal(
-//           TString infdataname="../output-pp-photon-7TeV-v2_v23_akPu3PF.root",
-//           TString infrefname="../output-py7TeV-pho30-v1_v23_akPu3PF.root",
-//           TString tag="akPu3PF"
-//           TString infdataname="../output-pp-photon-7TeV-v2_v23_ak5Calo.root",
-//           TString infrefname="../output-py7TeV-pho30-v1_v23_ak5Calo.root",
+           TString infdataname="../output-pp-photon-7TeV-v4_v24_akPu3PF.root",
+           TString infrefname="../output-py7TeV-pho30-v2_v24_akPu3PF.root",
+           TString tag="akPu3PF"
+//           TString infdataname="../output-pp-photon-7TeV-v4_v24_ak5Calo.root",
+//           TString infrefname="../output-py7TeV-pho30-v2_v24_ak5Calo.root",
 //           TString tag="ak5Calo"
-           TString infdataname="../output-hy18qcdpho50_v23_frac74_akPu3PF.root",
-           TString infrefname="../output-hy18qcdpho30_v23_frac74_akPu3PF.root",
-           TString tag=""
+//           TString infdataname="../output-pp-photon-7TeV-v4_v24_akPu3PF.root",
+//           TString infrefname="../output-py7TeV-pho30-v1_v24_akPu3PF.root",
+//           TString tag=""
 //           TString infdataname="../output-pp-photon-7TeV-v3_v23_akPu3PF.root",
 //           TString infrefname="../output-pp-photon-7TeV-v2_v23_akPu3PF.root",
 //           TString tag=""
@@ -39,26 +40,28 @@ void ppVal(
    TTree * tref = (TTree*)infref->Get("tgj");
    
    TCut selPhoSig = "sigmaIetaIeta<0.01&&sumIsol/0.9<1";
-   TCut selref = "offlSel&&photonEt>60&&jetEt>30&&abs(dphi)>3.14159*7./8"&&selPhoSig;
-   //TCut sel = selref&&"anaEvtSel";
-   TCut sel = selref;
+   //TCut selref = "offlSel&&photonEt>60&&jetEt>30&&abs(dphi)>3.14159*7./8"&&selPhoSig;
+   TCut selref = "photonEt>60&&jetEt>30&&abs(dphi)>3.14159*7./8"&&selPhoSig;
+   TCut sel = selref&&"anaEvtSel";
    cout << "compare: " << tdata->GetEntries(sel) << " " << tref->GetEntries(selref) << endl;
    
    
    
    // histograms
-//   TCanvas * c2 = new TCanvas("c2","c2",700,350);
-//   c2->Divide(2,1);
-//   c2->cd(1);
-//   Compare compx("xjg",tdata,tref,sel,selref,"");
-//   compx.Init(20,0,2);
-//   compx.Project("xgj>>");
-//   //compx.Draw(";x_{J,#gamma} = p_{T}^{J}/p_{T}^{#gamma};Event Fraction",0,0.5,"Iso. #gamma #hat{p}_{T} > 50 GeV/c "+tag,"Iso. #gamma #hat{p}_{T} > 30 GeV/c "+tag,1,0.5,0.8,0.9,0.9);   
-//   compx.Draw(";x_{J,#gamma} = p_{T}^{J}/p_{T}^{#gamma};Event Fraction",0,0.5,"ppv3 "+tag,"ppv2 "+tag,1,0.5,0.8,0.9,0.9);   
-//   c2->cd(2);
-//   TH1D * hXRat = compx.Ratio("Data/Mc");
-//   hXRat->SetAxisRange(0,2,"Y");
-//   hXRat->Draw();
+   TCanvas * c2 = new TCanvas("c2","c2",700,350);
+   c2->Divide(2,1);
+   c2->cd(1);
+   Compare compx("xjg",tdata,tref,sel,selref,"");
+   compx.Init(20,0,2);
+   compx.Project("xgj>>");
+   //compx.Draw(";x_{J,#gamma} = p_{T}^{J}/p_{T}^{#gamma};Event Fraction",0,0.5,"Iso. #gamma #hat{p}_{T} > 50 GeV/c "+tag,"Iso. #gamma #hat{p}_{T} > 30 GeV/c "+tag,1,0.5,0.8,0.9,0.9);   
+   compx.Draw(";x_{J,#gamma} = p_{T}^{J}/p_{T}^{#gamma};Event Fraction",0,0.5,"Data "+tag,"Iso. #gamma #hat{p}_{T} > 30 GeV/c "+tag,1,0.5,0.8,0.9,0.9);
+   cout << "Data mean x: " << compx.h1->GetMean() << " +/- " << compx.h1->GetMeanError() << endl;
+   cout << "MC mean x  : " << compx.h2->GetMean() << " +/- " << compx.h2->GetMeanError() << endl;
+   c2->cd(2);
+   TH1D * hXRat = compx.Ratio("Data/Mc");
+   hXRat->SetAxisRange(0,2,"Y");
+   hXRat->Draw();
 //
 //   //c2->Print("fig/pp/xjg_"+tag+".gif");
 //   //c2->Print("fig/mc/xjg_pthat30_vs_50_photonCut60.gif");
@@ -68,10 +71,9 @@ void ppVal(
    c3->Divide(2,1);
    c3->cd(1);
    Compare comppt("pt",tdata,tref,selPhoSig,selPhoSig,"");
-   comppt.normMode=0;
-   comppt.Init(50,40,200);
+   comppt.Init(50,60,200);
    comppt.Project("photonEt>>");
-   comppt.Draw(";p_{T}^{#gamma} (GeV/c);count",0,0,"Iso. #gamma #hat{p}_{T} > 50 GeV/c","Iso. #gamma #hat{p}_{T} > 30 GeV/c",1,0.5,0.8,0.9,0.9);   
+   comppt.Draw(";p_{T}^{#gamma} (GeV/c);count",0,0,"Data "+tag,"Iso. #gamma #hat{p}_{T} > 30 GeV/c "+tag,1,0.5,0.8,0.9,0.9);   
    c3->cd(2);
    TH1D * hPtRat = comppt.Ratio();
    hPtRat->SetAxisRange(0,2,"Y");
