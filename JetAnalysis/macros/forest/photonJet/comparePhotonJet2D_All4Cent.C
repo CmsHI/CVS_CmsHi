b6aa97d2ff5fa37311129d2109996db0588d488b
frankma <>
2012-02-21 23:08:44 +0000

    macros for inclusive jet ana

---
+++ JetAnalysis/macros/forest/photonJet/comparePhotonJet2D_All4Cent.C
@@ -0,0 +1,126 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TProfile.h"
+#include "TCanvas.h"
+#include "TCut.h"
+#include "TString.h"
+#include "TLine.h"
+#include "TLegend.h"
+#include <iostream>
+#include "commonUtility.h"
+using namespace std;
+
+void comparePhotonJet2D_All4Cent(
+                        int dataSrcType=1, // 0=mc, 1=data, 2=pp
+                        int isolScheme=0
+                        )
+{
+   TH1::SetDefaultSumw2();
+   TFile * inf;
+   if (dataSrcType==0) inf = TFile::Open("../output-hy18qcdpho30and50merge_v26_xsec_akPu3PF.root");
+   if (dataSrcType==1) inf = TFile::Open("../output-data-Photon-v7-noDuplicate_v26_akPu3PF.root");
+//   if (dataSrcType==2) inf = TFile::Open("../output-data-pp2010-prod3-photon_v18.root");
+//   if (dataSrcType==3) inf = TFile::Open("../output-pp7TeV-test2_v21.root");
+
+   TTree * tgj = (TTree*)inf->Get("tgj");
+   TCut selPre = "anaEvtSel";
+   TString dataSrcTitle="PbPb";
+   if (!dataSrcType) {
+      selPre = "offlSel";
+      dataSrcTitle="PYTHIA+HYDJET";
+   } else if (dataSrcType==1) {
+   } else if (dataSrcType==2) {
+      dataSrcTitle="pp 2.76 TeV";
+   } else if (dataSrcType==3) {
+      dataSrcTitle="pp 7 TeV";
+   }
+   TCut isolCut = "(cc4+cr4+ct4PtCut20)/0.9<1";
+   if (isolScheme==2) isolCut = "fisherIsol>0.3";
+   
+   TCut selPho = selPre&&"photonEt>60&&sigmaIetaIeta<0.01"&&isolCut;
+   TCut selPhoJet = selPho&&"inclJetPt>30&&acos(cos(inclJetPhi-photonPhi))>3.1415926*7./8";
+   TCut selPhoJetAllDPhi = selPho&&"inclJetPt>30&&acos(cos(inclJetPhi-photonPhi))>0.7";
+   float nPhoton=tgj->Draw("photonEt",selPho,"goff");
+   float nPhotonJet=tgj->Draw("inclJetPt/photonEt",selPhoJet,"goff");
+   cout << "photon Sel: " << selPho << " " << nPhoton << endl;
+   cout << "photonJet Sel: " << selPhoJet << " " << nPhotonJet << endl;
+
+   TString weight;
+   if (dataSrcType==0) weight="sampleWeight*weight";
+   
+   // Centrality bin cuts
+   const int nBin = 4;
+   float m[nBin+1] = {0,4,12,20,40};
+   vector<TCut> vcutCent;
+   for (int ib=0; ib<nBin; ++ib) vcutCent.push_back(Form("cBin>=%.1f&&cBin<%.1f",m[ib],m[ib+1]));
+   
+   TCanvas * c2_2 = new TCanvas("c2_2","",500,500);
+   tgj->Draw("cBin",selPho*weight,"");
+   
+   //////////////////////////////////////////////////////////////////////////
+   // Generic correlation plots
+   //////////////////////////////////////////////////////////////////////////
+
+//   TString tag = "ptj_ptg";
+//   TString var = "inclJetPt:photonEt";
+//   TString title = ";#gamma p_{T} (GeV/c);Jet p_{T} (GeV/c)";
+//   float nxbins=50, xmin=0, xmax=250, nybins=50, ymin=0, ymax=250;
+//   TCut mycut = selPhoJet;
+
+//   TString tag = "x_dphi";
+//   TString var = "inclJetPt/photonEt:acos(cos(inclJetPhi-photonPhi))";
+//   TString title = ";|#Delta#phi|;x_{J#gamma}";
+//   float nxbins=40, xmin=0, xmax=3.14159, nybins=40, ymin=0, ymax=2;
+//   TCut mycut = selPhoJetAllDPhi;
+
+//   TString tag = "jpt_dphi";
+//   TString var = "inclJetPt:acos(cos(inclJetPhi-photonPhi))";
+//   TString title = ";|#Delta#phi|;Jet p_{T} (GeV/c)";
+//   float nxbins=40, xmin=0, xmax=3.14159, nybins=40, ymin=0, ymax=200;
+//   TCut mycut = selPhoJetAllDPhi;
+
+//   TString tag = "ss_dphi";
+//   TString var = "sigmaIetaIeta:acos(cos(inclJetPhi-photonPhi))";
+//   TString title = ";|#Delta#phi|;#sigma_{#eta#eta}";
+//   float nxbins=40, xmin=0, xmax=3.14159, nybins=40, ymin=0, ymax=0.02;
+//   TCut mycut = selPre&&"photonEt>60&&inclJetPt>30&&acos(cos(inclJetPhi-photonPhi))>0.7"&&isolCut;;
+   
+   TString tag = "ss_isol";
+   TString var = "sigmaIetaIeta:sumIsol/0.9";
+   TString title = ";sumIsol;#sigma_{#eta#eta}";
+   float nxbins=40, xmin=-40, xmax=50, nybins=40, ymin=0, ymax=0.02;
+   TCut mycut = selPre&&"photonEt>60";
+
+   TCanvas * c4 = new TCanvas("c4","",1000,250);
+   c4->Divide(4,1);
+   cout << tag << " draw: " << var << endl;
+   for (int i=0; i<vcutCent.size(); ++i) {
+      c4->cd(i+1);
+      gPad->SetRightMargin(0.15);
+      int ib=vcutCent.size()-1-i;
+      TH2D * h2D = new TH2D(Form("h%s_%d",tag.Data(),ib),"",nxbins,xmin,xmax,nybins,ymin,ymax);
+      TCut selInBin = vcutCent[ib]&&mycut;
+      float nPhotonJetInBin = tgj->Project(h2D->GetName(),var,selInBin*weight,"E");
+      cout << TString(selInBin*weight) << " " << nPhotonJetInBin << endl;
+      h2D->SetTitle(title);
+      h2D->Draw("colz");
+      TLegend *leg2=new TLegend(0.138,0.81,0.38,0.94);
+      leg2->AddEntry(h2D,dataSrcTitle,"");
+      if (dataSrcType<=1) leg2->AddEntry(h2D,Form("%.0f to %.0f %%",m[ib]*2.5,m[ib+1]*2.5),"");
+      leg2->SetFillColor(0);
+      leg2->SetBorderSize(0);
+      leg2->SetFillStyle(0);
+      leg2->SetTextSize(0.04);
+      leg2->Draw();
+//      TLine * l0 = new TLine(3.1415926*7./8,0,3.1415926*7./8,2);
+      TLine * l0 = new TLine(1,ymin,1,ymax);
+      l0->SetLineColor(kRed);
+      l0->SetLineStyle(2);
+      l0->SetLineWidth(3);
+      l0->Draw();      
+   }
+   c4->Print(Form("fig/draw/%s_src%d_Isol%d.gif",tag.Data(),dataSrcType,isolScheme));
+   c4->Print(Form("fig/draw/%s_src%d_Isol%d.pdf",tag.Data(),dataSrcType,isolScheme));
+}
--- JetAnalysis/macros/forest/photonJet/illustrateRegions.C
+++ JetAnalysis/macros/forest/photonJet/illustrateRegions.C
@@ -17,23 +17,25 @@ void illustrateRegions(){
    gStyle->SetOptFit(0);
 
    TH1::SetDefaultSumw2();
-   TFile * inf = TFile::Open("../output-data-Photon-v7_v19.root");
+   TFile * inf = TFile::Open("../output-data-Photon-v7-noDuplicate_v26_akPu3PF.root");
    TTree * tgj = (TTree*)inf->Get("tgj");
    
-   //TCut sel = "(anaEvtSel&&cBin>=0&&cBin<4&&photonEt>60&&jetEt>30&&fisherIsol>0.3)";
-   TCut sel = "(anaEvtSel&&cBin>=0&&cBin<4&&photonEt>60&&jetEt>30&&sumIsol/0.9<1)";
-   TCut cutSigAll = "acos(cos(photonPhi-jetPhi))>2.0944&&sigmaIetaIeta<0.01";
-   TCut cutBkgDPhi = "acos(cos(photonPhi-jetPhi))>0.7&&acos(cos(photonPhi-jetPhi))<3.14/2&&sigmaIetaIeta<0.01";
-   TCut cutBkgSS = "acos(cos(photonPhi-jetPhi))>2.0944&&sigmaIetaIeta>0.01&&sigmaIetaIeta<0.017";
-   TCut cutBkgSSDPhi = "acos(cos(photonPhi-jetPhi))>0.7&&acos(cos(photonPhi-jetPhi))<3.14/2&&sigmaIetaIeta>0.01&&sigmaIetaIeta<0.017";
+   TCut sel = "(anaEvtSel&&cBin>=0&&cBin<4&&photonEt>60&&sumIsol/0.9<1&&inclJetPt>30&&acos(cos(photonPhi-inclJetPhi))>0.7)";
+//   TCut sel = "(anaEvtSel&&cBin>=0&&cBin<4&&photonEt>60&&fisherIsol>0.3&&inclJetPt>30&&acos(cos(photonPhi-inclJetPhi))>0.7)";
+   TCut cutSigAll = "acos(cos(photonPhi-inclJetPhi))>7./8*3.1415926&&sigmaIetaIeta<0.01";
+   TCut cutBkgDPhi = "acos(cos(photonPhi-inclJetPhi))>0.7&&acos(cos(photonPhi-inclJetPhi))<3.14/2&&sigmaIetaIeta<0.01";
+   TCut cutBkgSS = "acos(cos(photonPhi-inclJetPhi))>7./8*3.1415926&&sigmaIetaIeta>0.01&&sigmaIetaIeta<0.017";
+   TCut cutBkgSSDPhi = "acos(cos(photonPhi-inclJetPhi))>0.7&&acos(cos(photonPhi-inclJetPhi))<3.14/2&&sigmaIetaIeta>0.01&&sigmaIetaIeta<0.017";
    
-   float nSigAll = tgj->GetEntries(sel&&cutSigAll);
-   float nBkgDPhi = tgj->GetEntries(sel&&cutBkgDPhi);
-   float nBkgSS = tgj->GetEntries(sel&&cutBkgSS);
-   float nBkgSSDPhi = tgj->GetEntries(sel&&cutBkgSSDPhi);
+   float nSigAll = tgj->Draw("inclJetPt/photonEt",sel&&cutSigAll,"goff");
+   float nBkgDPhi = tgj->Draw("inclJetPt/photonEt",sel&&cutBkgDPhi,"goff");
+   float nBkgSS = tgj->Draw("inclJetPt/photonEt",sel&&cutBkgSS,"goff");
+   float nBkgSSDPhi = tgj->Draw("inclJetPt/photonEt",sel&&cutBkgSSDPhi,"goff");
+   
+   cout << TString(sel&&cutSigAll) << " " << nSigAll << endl;
    
    TH2D * h = new TH2D("h","",40,0,3.1415926,40,0,0.025);
-   tgj->Project("h","sigmaIetaIeta:acos(cos(jetPhi-photonPhi))",sel);
+   tgj->Project("h","sigmaIetaIeta:acos(cos(inclJetPhi-photonPhi))",sel);
    
    TCanvas * c2 = new TCanvas("c2","",500,500);
    c2->SetRightMargin(0.15);
@@ -41,7 +43,7 @@ void illustrateRegions(){
    h->SetAxisRange(0.004,0.02,"Y");
    h->Draw("colz");
 
-   TLegend *t2=new TLegend(0.31,0.15,0.59,0.29);
+   TLegend *t2=new TLegend(0.15,0.86,0.35,0.94);
    t2->AddEntry(h,"0-10%","");
    t2->SetFillColor(0);
    t2->SetBorderSize(0);
@@ -49,15 +51,19 @@ void illustrateRegions(){
    t2->SetTextSize(0.04);
    t2->Draw();
    
-   TLine * l0 = new TLine(2.0944,0.004,2.0944,0.02);
+   TLine * l0 = new TLine(3.1415926*7./8,0.004,3.1415926*7./8,0.02);
    l0->SetLineColor(kRed);
    l0->SetLineStyle(2);
    l0->SetLineWidth(3);
    l0->Draw();
    
-   TLine * l1 = new TLine(*l0);
-   l1->SetX1(0.7); l1->SetX2(0.7);
-   l1->Draw();
+//   TLine * l1 = new TLine(*l0);
+//   l1->SetX1(0.7); l1->SetX2(0.7);
+//   l1->Draw();
+
+//   TLine * l11 = new TLine(*l0);
+//   l11->SetX1(3.14159/2); l11->SetX2(3.14159/2);
+//   l11->Draw();
 
    TLine * l2 = new TLine(*l0);
    l2->SetX1(0); l2->SetX2(3.1415926);
@@ -68,8 +74,13 @@ void illustrateRegions(){
    l3->SetY1(0.017); l3->SetY2(0.017);
    l3->Draw();
    
-   drawText(Form("%.0f",nSigAll),0.685,0.288);
-   drawText(Form("%.0f",nBkgDPhi),0.48,0.288);
-   drawText(Form("%.0f",nBkgSS),0.71,0.62);
-   drawText(Form("%.0f",nBkgSSDPhi),0.48,0.62);
+   drawText(Form("%.0f",nSigAll),0.78,0.288);
+//   drawText(Form("%.0f",nBkgDPhi),0.4,0.288);
+   drawText(Form("%.0f",nBkgSS),0.78,0.62);
+//   drawText(Form("%.0f",nBkgSSDPhi),0.4,0.62);
+   
+//   c2->Print("fig/draw/IllustrateRegions_Isol0.pdf");
+//   c2->Print("fig/draw/IllustrateRegions_Isol0.gif");
+//   c2->Print("fig/draw/IllustrateRegions_Isol2.pdf");
+//   c2->Print("fig/draw/IllustrateRegions_Isol2.gif");
 }
