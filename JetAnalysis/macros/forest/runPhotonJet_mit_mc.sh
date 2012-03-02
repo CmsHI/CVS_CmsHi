c0a3bbc00eb332d865f70096ea0da3b8e410ccae
frankma <>
2012-03-02 01:46:25 +0000

    make more convenient for multiple pt hat samples

--- JetAnalysis/macros/forest/runPhotonJet_mit_mc.sh
+++ JetAnalysis/macros/forest/runPhotonJet_mit_mc.sh
@@ -1,27 +1,39 @@
 #!/bin/bash -
+# for xsec see,
+# https://twiki.cern.ch/twiki/bin/view/CMS/HiForestMC2011#Cross_section
 
 #
 # mit
 #
 for algo in akPu3PF; do
-# mixing classes
-#root -b -q analyzePhotonJet.C+'("akPu3PF","/mnt/hadoop/cms/store/user/jazzitup/hiForest/mergedFiles/forest_hydjetDrum_MB_20k.root","output-hy18mb_v28classes.root",0,1.,0,"","",1)'
-# mc embedded
-root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/merged/allQCDPhoton30_merged.root","norewt-output-hy18qcdpho30v2_v29_xsec.root",0,1.59,0,"","",0)'
-root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/merged/allQCDPhoton30_merged.root","output-hy18qcdpho30v2_v29_xsec.root",0,1.59,1,"norewt-output-hy18qcdpho30v2_v29_xsec.root","output-data-Photon-v7-noDuplicate_v29.root",0)'
-root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/merged/allQCDPhoton50_merged.root","norewt-output-hy18qcdpho50v2_v29_xsec.root",0,0.767,0,"","",0)'
-root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/merged/allQCDPhoton50_merged.root","output-hy18qcdpho50v2_v29_xsec.root",0,0.767,1,"norewt-output-hy18qcdpho50merge_v29_xsec.root","output-data-Photon-v7-noDuplicate_v29.root",0)'
-hadd -f output-hy18qcdpho30and50v2_v29_xsec_$algo.root output-hy18qcdpho30v2_v29_xsec_$algo.root output-hy18qcdpho50v2_v29_xsec_$algo.root
+  # mixing classes
+  #root -b -q analyzePhotonJet.C+'("akPu3PF","/mnt/hadoop/cms/store/user/jazzitup/hiForest/mergedFiles/forest_hydjetDrum_MB_20k.root","output-hy18mb_v28classes.root",0,1.,0,"","",1)'
 
-# mc mix
-#  root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/yinglu/MC_Production/allQCDPhoton/allQCDPhoton30/forest/allQCDPhoton30_50k_cent10_Merged.root","norewt-output-hy18qcdpho30merge_v28_xsec_mixmb.root",0,1.59,0,"","",2,"output-hy18mb_v28classes.root")'
-#  root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/yinglu/MC_Production/allQCDPhoton/allQCDPhoton30/forest/allQCDPhoton30_50k_cent10_Merged.root","output-hy18qcdpho30merge_v28_xsec_mixmb.root",0,1.59,1,"norewt-output-hy18qcdpho30merge_v28_xsec_mixmb.root","output-data-Photon-v7-noDuplicate_v28.root",2,"output-hy18mb_v28classes.root")'
-#  root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/yinglu/MC_Production/allQCDPhoton/allQCDPhoton50/forest/allQCDPhoton50_35k_cent10_Merged.root","norewt-output-hy18qcdpho50merge_v28_xsec_mixmb.root",0,0.767,0,"","",2,"output-hy18mb_v28classes.root")'
-#  root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/yinglu/MC_Production/allQCDPhoton/allQCDPhoton50/forest/allQCDPhoton50_35k_cent10_Merged.root","output-hy18qcdpho50merge_v28_xsec_mixmb.root",0,0.767,1,"norewt-output-hy18qcdpho50merge_v28_xsec_mixmb.root","output-data-Photon-v7-noDuplicate_v28.root",2,"output-hy18mb_v28classes.root")'
-#  hadd -f output-hy18qcdpho30and50merge_v28_xsec_mixmb_$algo.root output-hy18qcdpho30merge_v28_xsec_mixmb_$algo.root output-hy18qcdpho50merge_v28_xsec_mixmb_$algo.root
+  # mc embedded
+  cBinRefFile="output-data-Photon-v7-noDuplicate_v29.root"
+  mixFile="output-data-Photon-v7-noDuplicate_v29.root"
+  mergeCmd="hadd -f output-hy18qcdpho30and50v2_v29_xsec_$algo.root"
+  mergeMixCmd="hadd -f output-hy18qcdpho30and50v2_v29_xsec_mixmb_$algo.root"
+  for pthat in 30 50 80; do
+    sampleWt=1;
+    if [ $pthat -eq 30 ]; then sampleWt=1.59; fi
+    if [ $pthat -eq 50 ]; then sampleWt=0.767; fi
+    if [ $pthat -eq 80 ]; then sampleWt=0.172; fi
+    echo sampleWt: $sampleWt
+    inputFile="/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/merged/allQCDPhoton${pthat}_merged.root"
+    outputFile="output-hy18qcdpho${pthat}v2_v29_xsec.root"
+    root -b -q analyzePhotonJet.C+'("'$algo'","'$inputFile'","norewt-'$outputFile'",0,'$sampleWt',0,"","",0)'
+    root -b -q analyzePhotonJet.C+'("'$algo'","'$inputFile'","'$outputFile'",0,'$sampleWt',1,"norewt-'$outputFile'","'$cBinRefFile'",0)'
+    mergeCmd=$mergeCmd" output-hy18qcdpho${pthat}v2_v29_xsec_$algo.root"
+    # mc mix
+    outputMixFile="output-hy18qcdpho${pthat}v2_v29_xsec_mixmb.root"
+    root -b -q analyzePhotonJet.C+'("'$algo'","'$inputFile'","norewt-'$outputFile'",0,'$sampleWt',0,"","",2,"'$outputMixFile'")'
+    root -b -q analyzePhotonJet.C+'("'$algo'","'$inputFile'","'$outputFile'",0,'$sampleWt',1,"norewt-'$outputFile'","'$cBinRefFile'",2,"'$outputMixFile'")'
+  done
+  echo $mergeCmd
 
-# pp
-root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/ppForest/allQCDPhotons_OnlySignal_merged/allQCD_hiSignal2760GeV_pthat30_merged.root","output-qcdpho30hiSignalv2_v29_xsec.root",0,1.59,0)'
-root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/ppForest/allQCDPhotons_OnlySignal_merged/allQCD_hiSignal2760GeV_pthat50_merged.root","output-qcdpho50hiSignalv2_v29_xsec.root",0,0.767,0)'
-hadd -f output-qcdpho30and50hiSignalv2_v29_xsec_$algo.root output-qcdpho30hiSignalv2_v29_xsec_$algo.root output-qcdpho50hiSignalv2_v29_xsec_$algo.root
+  # pp
+  #root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/ppForest/allQCDPhotons_OnlySignal_merged/allQCD_hiSignal2760GeV_pthat30_merged.root","output-qcdpho30hiSignalv2_v29_xsec.root",0,1.59,0)'
+  #root -b -q analyzePhotonJet.C+'("'$algo'","/mnt/hadoop/cms/store/user/icali/Hydjet1.8/Z2/ppForest/allQCDPhotons_OnlySignal_merged/allQCD_hiSignal2760GeV_pthat50_merged.root","output-qcdpho50hiSignalv2_v29_xsec.root",0,0.767,0)'
+  #hadd -f output-qcdpho30and50hiSignalv2_v29_xsec_$algo.root output-qcdpho30hiSignalv2_v29_xsec_$algo.root output-qcdpho50hiSignalv2_v29_xsec_$algo.root
 done
