76696b4855e48bed204d7919d10faff3013a6bbc
yilmaz <>
2012-05-09 14:08:30 +0000

    More jet algos

--- JetAnalysis/python/PatAna_cff.py
+++ JetAnalysis/python/PatAna_cff.py
@@ -572,37 +572,6 @@ makeHeavyIonJets = cms.Sequence(icPu5patSequence +
 
                                 )
                                
-jetAnalyzers = cms.Sequence(
-    akPu1PFJetAnalyzer +
-    akPu2PFJetAnalyzer +
-    akPu3PFJetAnalyzer +
-    akPu4PFJetAnalyzer +
-    akPu5PFJetAnalyzer +
-    akPu6PFJetAnalyzer +
-    
-    akPu1CaloJetAnalyzer +
-    akPu2CaloJetAnalyzer +
-    akPu3CaloJetAnalyzer +
-    akPu4CaloJetAnalyzer +
-    akPu5CaloJetAnalyzer +
-    akPu6CaloJetAnalyzer +
-    
-    ak1PFJetAnalyzer +
-    ak2PFJetAnalyzer +
-    ak3PFJetAnalyzer +
-    ak4PFJetAnalyzer +
-    ak5PFJetAnalyzer +
-    ak6PFJetAnalyzer +
-    
-    ak1CaloJetAnalyzer +
-    ak2CaloJetAnalyzer +
-    ak3CaloJetAnalyzer +
-    ak4CaloJetAnalyzer +
-    ak5CaloJetAnalyzer +
-    ak6CaloJetAnalyzer
-    
-    )
-
 
 
 
