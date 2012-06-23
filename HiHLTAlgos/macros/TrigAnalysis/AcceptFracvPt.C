95627beaccb4b4aebf0bf90a4bff461d403a369e
frankma <>
2012-06-23 19:43:25 +0000

    updates for pPb mc

--- HiHLTAlgos/macros/TrigAnalysis/AcceptFracvPt.C
+++ HiHLTAlgos/macros/TrigAnalysis/AcceptFracvPt.C
@@ -13,6 +13,7 @@
 
 #include "commonUtility.h"
 #include "tgraphTools.C"
+#include "HisMath.C"
 
 using namespace std;
 
@@ -53,7 +54,7 @@ TGraphAsymmErrors *eff(TTree * t,TString var="nljet",TCut sel="",TCut cut="",TSt
 
 
 // Inputs tree information and graphs efficiency as a function of pt. 
-void trigEff()
+void AcceptFracvPt()
 {
    TString infile="/net/hisrv0001/home/yenjie/scratch/hiForest/prod/production/CMSSW_4_4_2_patch6/test/pPb/test_pPb.root";
    TChain * tree = new TChain("hltanalysis/HltTree");
@@ -62,38 +63,23 @@ void trigEff()
    tree->AddFriend("hiEvtAnalyzer/HiTree",infile);
    cout << "Total: " << tree->GetEntries() << endl;
    
-   TCut evtSel("HLT_HIMinBiasHfOrBSC_v1&&hiBin<40");
 //    TCut evtSel("HLT_HIMinBiasHfOrBSC_v1&&hiBin<40&&abs(jteta[0])<2");
+   TCut evtSel("hiBin<40");
+
+   TH1F *hJetPt = new TH1F("hJetPt",";leading jet p_{T} (GeV/c)",50,0,200);
+   float nSel = tree->Draw("jtpt[0]>>hJetPt",evtSel);
    
-   TGraphAsymmErrors *g0=0,*g1=0,*g2=0,*g3=0,*g4=0;
-     g0 = eff(tree,"jtpt[0]",evtSel,"HLT_HIJet55_v1","Jet55");
-   // g0 = eff(tree,"genpt[0]",evtSel,"HLT_HIJet35U","Jet35U");
-   // g0 = eff(tree,"jtpt[0]",evtSel,"L1_SingleJet30_BptxAND","Single Jet 30");
-   // g0 = eff(tree,"genpt[0]",evtSel,"L1_SingleJet30","Single Jet 30");
+   hJetPt->Scale(1./nSel);
    
-     //Draws Canvas
-   TCanvas *cTrigEff2 = new TCanvas("cTrigEff2","cTrigEff2",600,550);
-   TH1F *hTmp = new TH1F("hTmp","",nBin,bin);
-    hTmp->SetAxisRange(0,1.3,"Y");
-   hTmp->SetXTitle("Leading Jet p_{T} (GeV/c)"); 
-    //hTmp->SetXTitle("Gen Jet p_{T} (GeV/c)"); // Use for Monte Carlo
-   hTmp->SetYTitle("HLT  Eff. (Trigger/MB)");
-   // hTmp->SetYTitle(" HLT Eff. (Trigger/Generated Jets)"); // Use for Monte Carlo
-   handsomeTH1(hTmp);
-   hTmp->Draw();
-   g0->Draw("p");
-   
-   //Draws Legend
-   TLine *l = new TLine(bin[0],1,bin[nBin],1);
-   l->SetLineStyle(2);
-   l->Draw();
-   TLegend *t = new TLegend(0.251678,0.742366,.927852,0.9561);
-   //TLegend *t = new TLegend(0.236577,0.742366,1,0.956107); //Use for Monte Carlo
-//    t->SetHeader("All Physics |#eta|<2, All Centrality");
-   //("PYTHIA+HYDJET |#eta|<2, All Centrality"); //Use for Monte Carlo
-   t->SetBorderSize(0);
-   t->SetFillStyle(0);
-   t->Draw();
-   
-   cTrigEff2->Print("fig/pB/trigeff.gif"); // Must change name after each change in trigger selection.   
+   TCanvas * c2 = new TCanvas("c2","",500,500);
+   hJetPt->SetYTitle("a.n.");
+   hJetPt->Draw();
+   c2->Print("fig/pB/jet_pt.gif");
+
+   TCanvas * c3 = new TCanvas("c3","",500,500);
+   c3->SetLogy();
+   TH1D * hIntPt = integrateHist(hJetPt,false,-1);
+   hIntPt->SetTitle(";Jet p_{T} Threshold (GeV/c);Accept Fraction (from MB)");
+   hIntPt->Draw();
+   c3->Print("fig/pB/accept_fraction.gif");
 }
