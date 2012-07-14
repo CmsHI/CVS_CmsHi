007320ffa5436f6b1a0b14c30445004944ea36e2
yilmaz <>
2012-07-14 14:18:56 +0000

    Macros for guidance

---
+++ HiForest/V2/rootlogon.C
@@ -0,0 +1,63 @@
+{
+
+   gStyle->SetPalette(1,0);
+   gStyle->SetPadColor(0);
+   gStyle->SetPadBorderSize(0);
+   gStyle->SetPadBorderMode(0);
+   gStyle->SetCanvasColor(0);
+   gStyle->SetCanvasBorderMode(0);
+   gStyle->SetCanvasBorderSize(0);
+   gStyle->SetFrameBorderMode(0);
+   gStyle->SetFrameLineColor(0);
+   gStyle->SetTitleColor(0);
+   gStyle->SetTitleBorderSize(0);
+   gStyle->SetPalette(1,0);
+   gStyle->SetPadTickX(1);
+   gStyle->SetPadTickY(1);
+   gStyle->SetPadColor(0);
+   gStyle->SetPadBorderSize(0);
+   gStyle->SetPadBorderMode(0);
+   gStyle->SetCanvasColor(0);
+   gStyle->SetCanvasBorderMode(0);
+   gStyle->SetCanvasBorderSize(0);
+   gStyle->SetFrameBorderMode(0);
+   gStyle->SetFrameLineColor(0);
+   gStyle->SetFrameFillColor(0);
+   gStyle->SetTextFont(43);
+   gStyle->SetTextSize(22);
+   gStyle->SetLabelFont(43,"XYZ");
+   gStyle->SetTitleFont(43,"XYZ");
+   gStyle->SetTitleColor(0);
+   gStyle->SetTitleBorderSize(0);
+   gStyle->SetTitleXSize(32);
+   gStyle->SetTitleYSize(32);
+   gStyle->SetTitleXOffset(1.2);
+   gStyle->SetTitleYOffset(1.2);
+
+   gStyle->SetLabelSize(24,"XYZ");
+   gStyle->SetLabelOffset(0.01,"X");
+   gStyle->SetLabelOffset(0.01,"Y");
+   gStyle->SetTitleColor(1,"XYZ");
+   gStyle->SetHistLineColor(1);
+   gStyle->SetHistLineStyle(0);
+   gStyle->SetHistLineWidth(3);
+   gStyle->SetHistLineWidth(1);
+   //   gStyle->SetHistMarkerSize(.3);
+
+   gStyle->SetEndErrorSize(0);
+   gStyle->SetErrorX(0);
+   gStyle->SetMarkerStyle(20);
+   gStyle->SetMarkerSize(1.2);
+   gStyle->SetOptFit(1111);
+   gStyle->SetStatColor(0);
+   gStyle->SetStatBorderSize(1);
+   gStyle->SetOptStat(0);
+   gStyle->SetPadLeftMargin(0.17);
+   gStyle->SetPadBottomMargin(0.15);
+   gStyle->SetPadTopMargin(0.05);
+   gStyle->SetPadRightMargin(0.08);
+
+
+
+}
+
