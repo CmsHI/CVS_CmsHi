6d7248104e43cbcc87b5692450c54128ae298a38
dgulhan <>
2012-09-10 13:43:08 +0000

    muon process added

--- JetAnalysis/test/runForest_IterTrack_Data_44X_onRECO.py
+++ JetAnalysis/test/runForest_IterTrack_Data_44X_onRECO.py
@@ -21,14 +21,16 @@ ivars.register ('randomNumber',
 
 ivars.register ('doSkim',
                False, # default value
-               VarParsing.VarParsing.multiplicity.singleton, # singleton or list
+               VarParsing.VarParsing.multiplicity.singleton, # singleton or list 
                VarParsing.VarParsing.varType.bool,          # string, int, or float
                "whether to do skimming")
                   
 ivars.randomNumber = 1
-ivars.inputFiles = "file:/mnt/hadoop/cms/store/user/frankmalocal/data/reco/96A31CE6-1921-E111-B606-00A0D1E953AA.root"
-ivars.outputFile = 'output_data.root'
-ivars.maxEvents = -1
+# ivars.inputFiles = "file:/mnt/hadoop/cms/store/user/frankmalocal/data/reco/96A31CE6-1921-E111-B606-00A0D1E953AA.root"
+ivars.inputFiles = "file:/d00/dgulhan/testreco/96A31CE6-1921-E111-B606-00A0D1E953AA.root"
+ivars.outputFile = '/d00/dgulhan/output_data.root'
+# ivars.maxEvents = -1
+ivars.maxEvents = 1000
 
 ivars.parseArguments()
 
@@ -130,6 +132,11 @@ process.hiGenParticlesForJets.ignoreParticleIDs += cms.vuint32( 12,14,16)
 process.load("RecoLocalCalo/EcalRecAlgos/EcalSeverityLevelESProducer_cfi")
 process.load("RecoEcal.EgammaCoreTools.EcalNextToDeadChannelESProducer_cff")
 process.hiSelectedTrackHighPurity = process.hiSelectedTrackQuality.clone()
+
+##################### Muon Related
+process.load("MuTrig.HLTMuTree.hltMuTree_cfi")
+process.muonTree = process.hltMuTree.clone()
+process.muonTree.doGen = cms.untracked.bool(False)
 
 ##################### Particle Flow
 process.particleFlowTmp.postMuonCleaning = False
@@ -245,6 +252,7 @@ process.ana_step          = cms.Path(
                                       (process.anaTrack) +
                                       process.pfcandAnalyzer +
                                       process.hiEvtAnalyzer +
+                                      process.muonTree +
                                       process.HiForest
                                       )
 
