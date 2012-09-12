41101f9324d27569c6bbc774cccf9ceac8edd183
yilmaz <>
2012-09-12 15:28:43 +0000

    update to 526

--- JetAnalysis/scripts/setup_hiForest2_52x.sh
+++ JetAnalysis/scripts/setup_hiForest2_52x.sh
@@ -48,7 +48,7 @@ UserCode/ASYoon/PbPbTrackingTools/VertexConstraintProducer
 #      Yen-Jie: Evt plane configuration need check!
 cvs co -r HiForest_V02_04 -d       CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
 cvs co -r HiForest_V02_01 -d       CmsHi/PhotonAnalysis UserCode/CmsHi/PhotonAnalysis
-cvs co -r HiForest_V02_26 -d       CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
+cvs co -r HiForest_V02_30 -d       CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
 cvs co -r HiForest_V02_00 -d       HcalFilter UserCode/MTonjes/HcalFilter
 
 #PixelTrack
