cad1c5688449ea3e3cc4fe3cefe522b863da5e5e
yilmaz <>
2013-01-25 23:49:37 +0000

    jet pt cut

--- JetAnalysis/src/HiInclusiveJetAnalyzer.cc
+++ JetAnalysis/src/HiInclusiveJetAnalyzer.cc
@@ -455,6 +455,8 @@ HiInclusiveJetAnalyzer::analyze(const Event& iEvent,
 
    for(unsigned int j = 0; j < jets->size(); ++j){
      const reco::Jet& jet = (*jets)[j];     
+
+     if(jets_.jtpt[jets_.nref] < jetPtMin_) continue;
      if (useJEC_ && usePat_){
        jets_.rawpt[jets_.nref]=(*patjets)[j].correctedJet("Uncorrected").pt();
      }
