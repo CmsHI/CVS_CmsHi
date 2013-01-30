3a10f7fbecee15e4d402ecd8a963ca91b18f2e68
yilmaz <>
2013-01-30 15:39:33 +0000

    New beam scraping filter

--- JetAnalysis/test/runForest_pPb_Mix.py
+++ JetAnalysis/test/runForest_pPb_Mix.py
@@ -426,7 +426,7 @@ process.phfPosFilter1 = cms.Path(process.towersAboveThreshold+process.hfPosTower
 process.phfNegFilter1 = cms.Path(process.towersAboveThreshold+process.hfPosTowers+process.hfNegTowers+process.hfNegFilter)
 process.phltPixelClusterShapeFilter = cms.Path(process.siPixelRecHits+process.hltPixelClusterShapeFilter)
 process.pprimaryvertexFilter = cms.Path(process.primaryVertexFilter)
- 
+process.pBeamScrapingFilter=cms.Path(process.NoScraping) 
 
 # Customization
 from CmsHi.JetAnalysis.customise_cfi import *
@@ -464,8 +464,10 @@ process.schedule = cms.Schedule(
     process.phfPosFilter2,process.phfNegFilter2,
     process.phfPosFilter1,process.phfNegFilter1,
     process.phltPixelClusterShapeFilter,process.pprimaryvertexFilter,
+    process.pBeamScrapingFilter,
     process.hltAna,process.pAna
     )
+
 
 #process.schedule.extend(process.postHLTschedule)
 
