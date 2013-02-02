9732f36afb1ec27eaeeff82cd39d6f5a198a076e
yilmaz <>
2013-02-02 14:00:35 +0000

    update

--- JetAnalysis/macros/plotSignalMultiplicity.C
+++ JetAnalysis/macros/plotSignalMultiplicity.C
@@ -1,95 +1,168 @@
 #include "weightMix.C"
+#include "TProfile.h"
 
+static int centMode = 3;
+static string centName[] = {"N_{trk}^{offline}","HF+","N_{side}","N_{trk}^{low p_{T}}"};
 
-TProfile* getMult(int isample = 2, string var = "pt2/pt1"){
+TProfile* getMult(int isample = 2, string var = "xreco"){
 
   TH1::SetDefaultSumw2();
 
-  TChain* ntdijet;
-
-  if(var == "pt2/pt1"){
-    ntdijet = new TChain("ntmatch");
-  }else{
-    ntdijet = new TChain("ntdijet");
-  }
-
+  TChain* ntdijet = new TChain("ntdijet");
+  TChain* ntmatch = new TChain("ntmatch");
   TChain* ntevt = new TChain("ntevt");
   TChain* ntw;
 
+  const char* pPbFile = "data_pPb.root";
+  const char* PbPbFile = "data_PbPb.root";
+  const char* HydjetFile = "hydjet.root";
+  const char* HijingFile = "hijing.root";
+
   if(isample == 0){
-    ntdijet->Add("/d101/yetkin/analysis/d0128/data_pPb.root");
-    ntevt->Add("/d101/yetkin/analysis/d0128/data_pPb.root");
+    ntdijet->Add(pPbFile);
+    ntevt->Add(pPbFile);
   }
 
   if(isample == 1){
-    ntdijet->Add("/d101/yetkin/analysis/d0128/data_PbPb.root");
-    ntevt->Add("/d101/yetkin/analysis/d0128/data_PbPb.root");
+    ntdijet->Add(PbPbFile);
+    ntevt->Add(PbPbFile);
   }
 
   if(isample == 2){
     ntw = new TChain("ntw");
-
-    ntdijet->Add("/d101/yetkin/analysis/d0128/mix_hijing.root");
-    ntevt->Add("/d101/yetkin/analysis/d0128/mix_hijing.root");
-    ntw->Add("/d101/yetkin/analysis/d0128/mix_hijing.root");
+    ntmatch->Add(HijingFile);
+    ntdijet->Add(HijingFile);
+    ntevt->Add(HijingFile);
+    ntw->Add(HijingFile);
   }
 
   if(isample == 3){
     ntw = new TChain("ntw");
-
-    ntdijet->Add("/d101/yetkin/analysis/d0128/mix_hydjet.root");
-    ntevt->Add("/d101/yetkin/analysis/d0128/mix_hydjet.root");
-    ntw->Add("/d101/yetkin/analysis/d0128/mix_hydjet.root");
+    ntmatch->Add(HydjetFile);
+    ntdijet->Add(HydjetFile);
+    ntevt->Add(HydjetFile);
+    ntw->Add(HydjetFile);
   }
 
   string varName = "NNNNN";
   if(var == "ngen30") varName = "N_{GenJet}(p_{T}>30)";
   if(var == "njet30") varName = "N_{RecoJet}(p_{T}>30)";
-  if(var == "jtpt2/jtpt1") varName = "p_{T,2}/p_{T,1}";
-  if(var == "pt2/pt1"){
-    varName = "True (GenJet) p_{T,2}/p_{T,1}";    
+  if(var == "xreco") varName = "#LTp_{T,2}/p_{T,1}#GT";
+  if(var == "dphi") varName = "#LT#Delta#phi_{1,2}#GT";
+
+  if(var == "xgen"){
+    varName = "True (GenJet) #LTp_{T,2}/p_{T,1}#GT";    
   }
 
   ntdijet->AddFriend(ntevt);
-  if(isample > 1) ntdijet->AddFriend(ntw);
+  if(isample > 1){
+    ntdijet->AddFriend(ntw);
+    ntdijet->AddFriend(ntmatch);
+
+  }
 
   TH2D* h[20];
   TProfile* p[20];
 
-  int nmax = 200;
-  if(isample == 2) nmax = 500;
+  int nmax = 300;
+  if(centMode == 1) nmax = 100;
+  if(centMode == 2) nmax = 100;
+
+  double ymax = 10;
+
+  if(var == "dphi"){
+    ymax = pi;
+  }
+  if(var == "xreco" || var == "xgen"){
+    ymax = 1;
+  }
+
+  //  if(isample == 2) nmax = 500;
 
   for(int i = 0; i < 5; ++i){
-    h[i] = new TH2D(Form("h%d",i),"",nmax/10,0,nmax,10,0,10);
+    h[i] = new TH2D(Form("h%d",i),"",nmax/10,0,nmax,100,0,ymax);
   }
   
   TH2D* hn = new TH2D(Form("hn%d",isample),"",nmax/10,0,nmax,100,0,2.);
 
-  TCut dijet("jtpt1 > 120 && jtpt2 > 30 && acos(cos(jtphi1-jtphi2))> 2.0944");
+  TCut dijet("jtpt1 > 120 && jtpt2 > 30");
+
+  if(var != "dphi") dijet = dijet&&"acos(cos(jtphi1-jtphi2))> 2.0944";
   TCut weight("weight");
   if(isample < 2) weight = "jtpt1 > -999";
 
+  if(centMode == 0){
+    ntdijet->SetAlias("cent","ntrk");
+    ntdijet->SetAlias("gens","nps");
+    ntdijet->SetAlias("genb","npb");
+  }
+
+  if(centMode == 1){
+    ntdijet->SetAlias("cent","hf");
+    ntdijet->SetAlias("gens","hfs");
+    ntdijet->SetAlias("genb","hfb");
+  }
+
+  if(centMode == 2){
+    ntdijet->SetAlias("cent","nside");
+    ntdijet->SetAlias("gens","nls");
+    ntdijet->SetAlias("genb","nlb");
+  }
+
+  if(centMode == 3){
+    ntdijet->SetAlias("cent","nls+nlb");
+    ntdijet->SetAlias("gens","nls");
+    ntdijet->SetAlias("genb","nlb");
+  }
+
+  ntdijet->SetAlias("gen","gens+genb");
+
+
+  ntdijet->SetAlias("xgen","pt2/pt1");
+  ntdijet->SetAlias("xreco","jtpt2/jtpt1");
+  ntdijet->SetAlias("dphi","acos(cos(jtphi1-jtphi2))");
+  ntdijet->SetAlias("","");
+  ntdijet->SetAlias("","");
 
   ntdijet->SetAlias("var",var.data());
 
 
+  if(var == "jtpt2/jtpt1") var = "xreco";
+  if(var == "pt2/pt1") var = "xgen";
+  if(var == "acos(cos(jtphi1-jtphi2))") var = "dphi";
+
+
   TCanvas* c2 = new TCanvas("c2","",600,600);
-  ntdijet->Draw(Form("var:ntrk>>h%d",0),weight*dijet,"colz");
+  ntdijet->Draw(Form("var:cent>>h%d",0),weight*dijet,"colz");
+  c2->Print(Form("figure_c2_%s_%d.pdf",var.data(),isample));
+
 
   if(isample > 1){
 
     TCanvas* c1 = new TCanvas("c1","",600,600);
-    ntdijet->Draw(Form("nps/npb:ntrk>>hn%d",isample),weight*dijet,"colz");
+    ntdijet->Draw(Form("gens/genb:cent>>hn%d",isample),weight*dijet,"colz");
+    hn->SetTitle(Form(";%s;N_{signal particle}/N_{bkg particle}",centName[centMode].data()));
+    hn->GetXaxis()->CenterTitle();
+    hn->GetYaxis()->CenterTitle();
+
+    hn->Draw("colz");
+
+
     hn->ProfileX(Form("phn_%d",isample))->Draw("same");
+    c1->Print(Form("figure_c1_%s_%d.pdf",var.data(),isample));
     
     TCanvas* c3 = new TCanvas("c3","",600,600);
-    ntdijet->Draw(Form("var:npb+nps>>h%d",1),weight*dijet,"colz");
+    ntdijet->Draw(Form("var:genb+gens>>h%d",1),weight*dijet,"colz");
+    c3->Print(Form("figure_c3_%s_%d.pdf",var.data(),isample));
+
     
     TCanvas* c4 = new TCanvas("c4","",600,600);
-    ntdijet->Draw(Form("var:nps>>h%d",2),weight*dijet,"colz");
-    
+    ntdijet->Draw(Form("var:gens>>h%d",2),weight*dijet,"colz");
+    c4->Print(Form("figure_c4_%s_%d.pdf",var.data(),isample));
+
     TCanvas* c5 = new TCanvas("c5","",600,600);
-    ntdijet->Draw(Form("var:npb>>h%d",3),weight*dijet,"colz");
+    ntdijet->Draw(Form("var:genb>>h%d",3),weight*dijet,"colz");
+    c5->Print(Form("figure_c5_%s_%d.pdf",var.data(),isample));
   
   }
 
@@ -107,26 +180,69 @@ TProfile* getMult(int isample = 2, string var = "pt2/pt1"){
   p[1]->SetMaximum(5);
   p[1]->SetMinimum(0);
 
+  if(var == "xreco" || var == "xgen"){
+    p[1]->SetMaximum(1);
+  }
+  if(var == "dphi"){
+    p[1]->SetMaximum(pi);
+    p[1]->SetMinimum(pi-pi/3);
+
+  }
+  if(var == "nps" || var == "npb"){
+    p[1]->SetMaximum(50);
+  }
+
+  p[1]->GetXaxis()->SetTitle("N_{?}");
+  p[0]->GetXaxis()->SetTitle(centName[centMode].data());
+
+  for(int i = 0; i < 4; ++i){
+    p[i]->GetYaxis()->SetTitle(varName.data());
+    p[i]->GetXaxis()->CenterTitle();
+    p[i]->GetYaxis()->CenterTitle();
+  }
+
+
   p[1]->Draw("hist");
   p[0]->Draw("same");
 
   p[2]->Draw("same");
   p[3]->Draw("same");
 
-  p[0]->GetXaxis()->SetTitle("N_{trk}^{offline}");
-  p[0]->GetYaxis()->SetTitle(varName.data());
-  p[0]->GetXaxis()->CenterTitle();
-  p[0]->GetYaxis()->CenterTitle();
+  TLegend *t3=new TLegend(0.26,0.2,0.53,0.45);
+  t3->SetFillColor(0);
+  t3->SetBorderSize(0);
+  t3->SetFillStyle(0);
+  t3->SetTextFont(63);
+  t3->SetTextSize(20);
+  if(centMode == 0){
+    t3->AddEntry(p[1],Form("N = %s","N_{gen}"),"l");
+    t3->AddEntry(p[0],Form("N = %s","N_{trk}"),"p");
+    t3->AddEntry(p[2],Form("N = %s","N_{signal}"),"p");
+    t3->AddEntry(p[3],Form("N = %s","N_{bkg}"),"p");
+  }
+  if(centMode == 1){
+    t3->AddEntry(p[1],Form("N = %s","E_{T}^{HF}_{gen}"),"l");
+    t3->AddEntry(p[0],Form("N = %s","E_{T}^{HF}_{reco}"),"p");
+    t3->AddEntry(p[2],Form("N = %s","E_{T}^{HF}_{signal}"),"p");
+    t3->AddEntry(p[3],Form("N = %s","E_{T}^{HF}_{bkg}"),"p");
+  }
+
+  t3->Draw();
+
+  cc1->Print(Form("figure_cc1_%s_%d.pdf",var.data(),isample));
+
 
   return p[0];
 
 }
 
 
-void plotSignalMultiplicity(string var = "njet30"){
+void plotVariable(string var = "jtpt2/jtpt1"){
 
-  //  getSignalBias(2,var);
-  //  return;
+   //   var = "nps";
+   //   var = "npb";
+
+  TCanvas* cc2 = new TCanvas("cc2","",600,600);
 
   TProfile* p[4];
 
@@ -136,6 +252,7 @@ void plotSignalMultiplicity(string var = "njet30"){
     p[i]->SetDirectory(0);
   }
 
+  p[1]->GetXaxis()->SetTitle(centName[centMode].data());
 
   p[1]->SetMarkerStyle(25);
   p[1]->SetMarkerColor(4);
@@ -143,12 +260,27 @@ void plotSignalMultiplicity(string var = "njet30"){
 
   p[3]->SetLineColor(4);
 
+  p[1]->SetMaximum(5);
+  p[1]->SetMinimum(0);
+  if(var == "xreco" || var == "xgen"){
+    p[1]->SetMaximum(1);
+  }
+  if(var == "dphi"){
+    p[1]->SetMaximum(pi);
+    p[1]->SetMinimum(pi-pi/3);
+
+  }
+  if(var == "nps" || var == "npb"){
+    p[1]->SetMaximum(250);
+  }
+
+  cc2->cd();
   p[1]->Draw("");
   p[0]->Draw("same");
   p[2]->Draw("hist same");
   p[3]->Draw("hist same");
 
-  TLegend *t3=new TLegend(0.26,0.6,0.53,0.85);
+  TLegend *t3=new TLegend(0.26,0.2,0.53,0.45);
   t3->SetFillColor(0);
   t3->SetBorderSize(0);
   t3->SetFillStyle(0);
@@ -161,10 +293,94 @@ void plotSignalMultiplicity(string var = "njet30"){
 
   t3->Draw();
 
+  if(var == "jtpt2/jtpt1") var = "xreco";
+  if(var == "pt2/pt1") var = "xgen";
+  if(var == "acos(cos(jtphi1-jtphi2))") var = "dphi";
+
+  cc2->Print(Form("figure_cc0_%s.pdf",var.data()));
+
+
+  TCanvas* cc3 = new TCanvas("cc3","",600,600);
+
+  p[0]->GetXaxis()->SetTitle(centName[centMode].data());
+
+  p[0]->Draw("");
+  p[2]->Draw("hist same");
+
+  t3=new TLegend(0.26,0.2,0.53,0.45);
+  t3->SetFillColor(0);
+  t3->SetBorderSize(0);
+  t3->SetFillStyle(0);
+  t3->SetTextFont(63);
+  t3->SetTextSize(20);
+  t3->AddEntry(p[0],"pPb data","p");
+  t3->AddEntry(p[2],"pPb Hijing + Pythia","l");
+
+  t3->Draw();
+
+  cc3->Print(Form("figure_cc3_pPb_%s.pdf",var.data()));
+
+  TCanvas* cc4 = new TCanvas("cc4","",600,600);
+  if(centMode == 0){
+    p[1]->GetXaxis()->SetTitle("N_{trk}^{heavy ion tracking}");
+  }
+  if(centMode == 1){
+    p[1]->GetXaxis()->SetTitle("E_{T}^{HF+}_{heavy ion reco}");
+  }
+
+  p[1]->Draw("");
+  p[3]->Draw("hist same");
+
+  t3=new TLegend(0.26,0.2,0.53,0.45);
+  t3->SetFillColor(0);
+  t3->SetBorderSize(0);
+  t3->SetFillStyle(0);
+  t3->SetTextFont(63);
+  t3->SetTextSize(20);
+  t3->AddEntry(p[1],"PbPb data","p");
+  t3->AddEntry(p[3],"PbPb Hydjet + Pythia","l");
+
+  t3->Draw();
+
+  cc4->Print(Form("figure_cc4_PbPb_%s.pdf",var.data()));
+
+
+
+
+
+
 
 }
 
 
+
+void plotSignalMultiplicity(){
+
+  string var = "xreco";
+  plotVariable(var);
+
+  return;
+
+  var = "xgen";
+  plotVariable(var);
+
+
+  var = "dphi";
+  plotVariable(var);
+
+  var = "ngen30";
+  plotVariable(var);
+
+  var = "njet30";
+  plotVariable(var);
+
+  var = "nps";
+  plotVariable(var);
+  var = "npb";
+  plotVariable(var);
+
+
+}
 
 
 
--- JetAnalysis/macros/weightMix.C
+++ JetAnalysis/macros/weightMix.C
@@ -54,9 +54,10 @@ static bool reweightCentrality = 1;
 static const double pi = TMath::Pi();
 
 
-TCut weightSample(
+void weightSample(
 		  bool mix = 1,
-		  bool PbPb2011 = 0
+		  bool PbPb2011 = 0,
+		  int entryType = 2
 		  ){
 
 
@@ -64,66 +65,60 @@ TCut weightSample(
   bool validate = 1;
   //  validate = 0;
 
-  double xs15 = 2.03E-01 * 1000000;
-  double xs30 = 1.079E-02 * 1000000;
-  double xs50 = 1.021E-03 * 1000000;
-  double xs80 = 9.913E-05 * 1000000;
-  double xs100 = 3.069E-05 * 1000000;
-  double xs120 = 1.128E-05 * 1000000;
-  double xs170 = 1.470E-06 * 1000000;
-  double xs200 = 5.310E-07 * 1000000;
-  double xs250 = 1.192E-07 * 1000000;
-  double xs300 = 3.176E-08 * 1000000;
+  int Npt = 9;
+
+  double pthatBins2013[] = {15,30,50,80,120,170,220,280,370,9999};
+  double xs2013[] = {5.335E-01,3.378E-02,3.778E-03,4.412E-04,6.147E-05,1.018E-05,2.477E-06,6.160E-07,1.088E-07};
+
+  double pthatBins2011[] = {15,30,50,80,100,120,170,200,250,300,9999};
+  double xs2011[] = {2.03E-01,1.079E-02,1.021E-03,9.913E-05,3.069E-05,1.128E-05,1.470E-06,5.310E-07,1.192E-07,3.176E-08};
+
+  double *xs;
+  double *pthats;
+  int nev[20];
+  int n[20];
 
   if(PbPb2011){
-    xs15 = 2.03E-01 * 1000000;
-    xs30 = 1.079E-02 * 1000000;
-    xs50 = 1.021E-03 * 1000000;
-    xs80 = 9.913E-05 * 1000000;
-    xs100 = 3.069E-05 * 1000000;
-    xs120 = 1.128E-05 * 1000000;
-    xs170 = 1.470E-06 * 1000000;
-    xs200 = 5.310E-07 * 1000000;
-    xs250 = 1.192E-07 * 1000000;
-    xs300 = 3.176E-08 * 1000000;
+    Npt = 10;
+    xs = xs2011;
+    pthats = pthatBins2011;
+  }else{
+    xs = xs2013;
+    pthats = pthatBins2013;
   }
 
-  xs15  -= xs30;
-  xs30  -= xs50;
-  xs50  -= xs80;
-  xs80  -= xs100;
-  xs100 -= xs120;
-  xs120 -= xs170;
-  xs170 -= xs200;
-  xs200 -= xs250;
-  xs250 -= xs300;
+  for(int i = 0; i < Npt-1; ++i){
+    xs[i] -= xs[i+1];
+  }
 
   TChain* nt;
   TFile* outf;
+
+  string ntwNames[] = {"ntw","ntjetw","nttrkw"};
+  string ntevtNames[] = {"ntevt","ntjet","nttrk"};
+
   if(mix){
     if(PbPb2011){
-      outf = new TFile("weights_hydjet.root","recreate");
-      nt = new TChain("ntevt");
+      outf = new TFile(Form("weights_hydjet_i%d.root",entryType),"recreate");
+      nt = new TChain(ntevtNames[entryType].data());
     }else{
-      outf = new TFile("weights_hijing.root","recreate");
+      outf = new TFile(Form("weights_hijing_i%d.root",entryType),"recreate");
       if(0){
 	nt = new TChain("ak5PFJetAnalyzer/t");
       }else{
-	nt = new TChain("ntevt");
+	nt = new TChain(ntevtNames[entryType].data());
       }
     }
   }else{
-    outf = new TFile("weights_signal.root","recreate");
+    outf = new TFile(Form("weights_signal_i%d.root",entryType),"recreate");
     nt = new TChain("ak5PFJetAnalyzer/t");
   }
 
-  TNtuple* ntw = new TNtuple("ntw","","weight:ptweight:cweight:varpthat");
+  TNtuple* ntw = new TNtuple(Form("%s",ntwNames[entryType].data()),"","weight:ptweight:cweight:varpthat");
 
   float varpthat, varbin;
   //  TNtuple* nt = (TNtuple*)(new TFile("mix.root"))->Get("nt");
 
-
-  int pthats[] = {15,30,50,80,120,170};
 
   if(PbPb2011){
     nt->AddFile("mix_hydjet.root");
@@ -134,10 +129,10 @@ TCut weightSample(
 	int forestversion = 2;
 	if(ip  >= 5) forestversion = 1;	
 	if(mix){	  
-	  nt->AddFile(Form("root://eoscms//eos/cms/store/caf/user/dgulhan/pPb/HP03/prod09/Hijing_Pythia_pt%d/HiForest_v55_v04_merged01/pt%d_HP03_prod09_merged_forest_0.root",pthats[ip],pthats[ip]));	  	  
+	  nt->AddFile(Form("root://eoscms//eos/cms/store/caf/user/dgulhan/pPb/HP03/prod09/Hijing_Pythia_pt%d/HiForest_v55_v04_merged01/pt%d_HP03_prod09_merged_forest_0.root",(int)pthats[ip],(int)pthats[ip]));	  	  
 	}else{      
 	  forestversion =1;
-	  nt->AddFile(Form("root://eoscms//eos/cms/store/caf/user/dgulhan/pPb_A03/Signal_Pythia%d/JEC_merged02/merged_forest_0.root",pthats[ip]));      
+	  nt->AddFile(Form("root://eoscms//eos/cms/store/caf/user/dgulhan/pPb_A03/Signal_Pythia%d/JEC_merged02/merged_forest_0.root",(int)pthats[ip]));      
 	}	
       }
     }else{
@@ -153,157 +148,36 @@ TCut weightSample(
     varbin = 0;
   }
 
-  int nev= 0, nev15, nev30 = 0, nev50 = 0, nev80 = 0, nev100 = 0, nev120 = 0, nev170 = 0, nev200 = 0, nev250 = 0, nev300 = 0;
-  int n = 0, n15, n30 = 0, n50 = 0, n80 = 0, n100 = 0, n120 = 0, n170 = 0, n200 = 0, n250 = 0, n300 = 0;
+  for(int i = 0; i < Npt; ++i){
+    TCut pthatCut(Form("pthat >= %f && pthat < %f",pthats[i],pthats[i+1]));
 
-  nev = nt->GetEntries();
 
-  TCut pthat15("pthat >= 15 && pthat < 30");
-  TCut pthat30("pthat >= 30 && pthat < 50");
-  TCut pthat50("pthat >= 50 && pthat < 80");
-  TCut pthat80("pthat >= 80 && pthat < 100");
-  TCut pthat100("pthat >= 100 && pthat < 120");
-  TCut pthat120("pthat >= 120 && pthat < 170");
-  TCut pthat170("pthat >= 170 && pthat < 200");
-  TCut pthat200("pthat >= 200 && pthat < 250");
-  TCut pthat250("pthat >= 250 && pthat < 300");
-  TCut pthat300("pthat >= 300");
-
-  n = nev;
-  n15 = nt->GetEntries(pthat15);
-  n30 = nt->GetEntries(pthat30);
-  n50 = nt->GetEntries(pthat50);
-  n80 = nt->GetEntries(pthat80);
-  n120 = nt->GetEntries(pthat120);
-  n100 = nt->GetEntries(pthat100);
-  n170 = nt->GetEntries(pthat170);
-  n200 = nt->GetEntries(pthat200);
-  n250 = nt->GetEntries(pthat250);
-  n300 = nt->GetEntries(pthat300);
-
-  TCut w15(Form("%f/%d",xs15,n15));
-  TCut w30(Form("%f/%d",xs30,n30));
-  TCut w50(Form("%f/%d",xs50,n50));
-  TCut w80(Form("%f/%d",xs80,n80));
-  TCut w100(Form("%f/%d",xs100,n100));
-  TCut w120(Form("%f/%d",xs120,n120));
-  TCut w170(Form("%f/%d",xs170,n170));
-  TCut w200(Form("%f/%d",xs200,n200));
-  TCut w250(Form("%f/%d",xs250,n250));
-  TCut w300(Form("%f/%d",xs300,n300));
-
-  w15 = w15*(pthat15);
-  w30 = w30*(pthat30);
-  w50 = w50*(pthat50);
-  w80 = w80*(pthat80);
-  w100 = w100*(pthat100);
-  w120 = w120*(pthat120);
-  w170 = w170*(pthat170);
-  w200 = w200*(pthat200);
-  w250 = w250*(pthat250);
-  w300 = w300*(pthat300);
+    n[i] = nt->GetEntries(pthatCut);
+  }
 
   for(int ie = 0; ie < nt->GetEntries(); ++ie){
     nt->GetEntry(ie);
     double pthatweight = 0;
-    if(n15 > 0 && varpthat >= 15) pthatweight = xs15/n15;
-    if(n30 > 0 && varpthat >= 30) pthatweight = xs30/n30;
-    if(n50 > 0 && varpthat >= 50) pthatweight = xs50/n50;
-    if(n80 > 0 && varpthat >= 80) pthatweight = xs80/n80;
-    if(n100 > 0 && varpthat >= 100) pthatweight = xs100/n100;
-    if(n120 > 0 && varpthat >= 120) pthatweight = xs120/n120;
-    if(n170 > 0 && varpthat >= 170) pthatweight = xs170/n170;
-    if(n200 > 0 && varpthat >= 200) pthatweight = xs200/n200;
-    if(n250 > 0 && varpthat >= 250) pthatweight = xs250/n250;
-    if(n300 > 0 && varpthat >= 300) pthatweight = xs300/n300;
 
-    //    cout<<"pthat : "<<varpthat<<"  weight : "<<pthatweight<<endl;
+    for(int i = 0; i < Npt; ++i){
+      if(n[i] > 0 && varpthat >= pthats[i]) pthatweight = xs[i]/n[i]*10000;
+    }
 
     double cweight = exp(-  pow(varbin+1.11957e+01,2)  /    pow(1.34120e+01,2) / 2);
     ntw->Fill(pthatweight*cweight,pthatweight,cweight,varpthat);
-  }
-
-  TCut weight(Form("%s + %s + %s + %s",(const char*)w80,(const char*)w120,(const char*)w170,(const char*)w200));
-
-  if(reweightCentrality) weight = Form("(%s)*%s",(const char*)weight,
-				       "exp(-  pow(bin+1.11957e+01,2)  /    pow(1.34120e+01,2) / 2)");
-  
-  if(validate){
-
-    TCanvas* c1 = new TCanvas("c1","",600,600);
-    TH1D* hMix = new TH1D("hMix",";p_{T}^{Leading RecoJet};",120,0,600);
-
-  cout<<"Entries : "<<nt->GetEntries(weight)<<endl;
-  nt->Draw("pt1>>hMix",weight);
-
-  THStack* h = new THStack("h",";p_{T}^{Leading RecoJet}; Weighted Entries");
-  TH1D* h80 = new TH1D("h80",";p_{T}^{Leading RecoJet};",380,120,600);
-  TH1D* h120 = new TH1D("h120",";p_{T}^{Leading RecoJet};",380,120,600);
-  TH1D* h170 = new TH1D("h170",";p_{T}^{Leading RecoJet};",380,120,600);
-  TH1D* h200 = new TH1D("h200",";p_{T}^{Leading RecoJet};",380,120,600);
-
-  c1->SetLogy();
-  //  hMix->SetAxisRange(120,500);
-  //  hMix->SetMinimum(0.00001);
-  hMix->Draw();
-
-  TCanvas* c2 = new TCanvas("c2","",600,600);
-  c2->SetLogy();
-  nt->Draw("pt1>>h80",w80);
-  nt->Draw("pt1>>h120",w120);
-  nt->Draw("pt1>>h170",w170);
-  nt->Draw("pt1>>h200",w200);
-
-  h80->Rebin(5);
-  h120->Rebin(5);
-  h170->Rebin(5);
-  h200->Rebin(5);
-
-  h80->SetFillColor(12);
-  h120->SetFillColor(4);
-  h170->SetFillColor(5);
-  h200->SetFillColor(2);
-
-  h80->SetFillStyle(1);
-  h120->SetFillStyle(1);
-  h170->SetFillStyle(1);
-  h200->SetFillStyle(1);
-
-  h->Add(h80);
-  h->Add(h120);
-  h->Add(h170);
-  h->Add(h200);
-
-  h->Draw("hist");
-
-  weightString = TString((const char*)weight);
-  new TCanvas();
-
-  nt->Draw("bin",weightString.Data());
-
-  new TCanvas();
-
-
-  TF1* f = new TF1("f","exp(-  pow(x+1.11957e+01,2)  /    pow(1.34120e+01,2) / 2)",0,40);
-  f->Draw();
-
   }
 
   outf->cd();
   ntw->Write();
   outf->Write();
 
-  cout<<(const char*)weight<<endl;
-  weightString = TString((const char*)weight);
-
-
-  return weight;
 }
 
 void weightMix(){
 
-  //  weightSample(1,0);
-  weightSample(1,1);
+  weightSample(1,0,2);
+  weightSample(1,0,1);
+  weightSample(1,0,0);
 
 }
 
