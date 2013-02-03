6d3cd5ef002d85b2baeb88bb25e921fec7a38dc2
kumarv <>
2013-02-03 05:48:12 +0000

    Read Single Mu Tree for Trigger Eff

--- HiDiMuonAna/DiMuon/test/test538.py
+++ HiDiMuonAna/DiMuon/test/test538.py
@@ -37,7 +37,7 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(100)
     )
 
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
