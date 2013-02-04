ed07b5d9abcb4389d422a1451d0b6696747d66d3
yilmaz <>
2013-02-04 22:10:21 +0000

    update

---
+++ JetAnalysis/macros/makeAllPlots.C
@@ -0,0 +1,24 @@
+
+
+#include "plotFigure.C"
+
+void makeAllPlots(){
+
+  plotFigure(0);
+  plotFigure(1);
+  plotFigure(3);
+  plotFigure(8);
+  plotFigure(9);
+
+  plotFigure(11);
+  plotFigure(12);
+
+  plotFigure(21);
+  plotFigure(22);
+
+
+
+}
+
+
+
